bbcce370875e0284aa819a726652bc23155b8ae2
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-04-16 12:16:56 -0700

    updated test for demo spill run

--- js/views/model/map.js
+++ js/views/model/map.js
@@ -11,6 +11,32 @@ define([
         id: 'map',
         full: false,
         width: '70%',
+        index: 0,
+        step: [],
+
+        icon_style: new ol.style.Circle({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, .2)'
+            }),
+            radius: 4,
+            stroke: new ol.style.Stroke({
+                color: '#0099cc'
+            })
+        }),
+
+        part_icon_style: new ol.style.Circle({
+            fill: new ol.style.Fill({
+                color: 'rgba(0, 0, 0, .75)'
+            }),
+            radius: 1,
+            stroke: new ol.style.Stroke({
+                color: 'rgba(0, 0, 0, 1)'
+            })
+        }),
+
+        events: {
+            'click .play': 'play'
+        },
 
         initialize: function(){
             this.render();
@@ -38,24 +64,9 @@ define([
         },
 
         renderMap: function(){
-            var icon = new ol.style.Circle({
-                fill: new ol.style.Fill({
-                    color: 'rgba(255, 255, 255, .2)'
-                }),
-                radius: 4,
-                stroke: new ol.style.Stroke({
-                    color: '#0099cc'
-                })
-            });
-            var part_icon = new ol.style.Circle({
-                fill: new ol.style.Fill({
-                    color: 'rgba(0, 0, 0, .75)'
-                }),
-                radius: 1,
-                stroke: new ol.style.Stroke({
-                    color: 'rgba(0, 0, 0, 1)'
-                })
-            });
+            var icon_style = this.icon_style;
+            var part_icon_style = this.part_icon_style;
+
             webgnome.map = new ol.Map({
                 controls: ol.control.defaults().extend([
                     new ol.control.MeasureRuler(),
@@ -79,30 +90,6 @@ define([
                             projection: 'EPSG:3857',
                             url: '/sw_TX_jetties.json',
                         })
-                    }),
-                    new ol.layer.Vector({
-                        source: new ol.source.GeoJSON({
-                            projection: 'EPSG:3857',
-                            url: '/COOPSu_NGOFS-step-1.json'
-                        }),
-                        style: function(feature, resolution){
-                            if(resolution < 200){
-                                return [new ol.style.Style({
-                                    image: icon
-                                })];
-                            }
-                        }
-                    }),
-                    new ol.layer.Vector({
-                        source: new ol.source.GeoJSON({
-                            projection: 'EPSG:3857',
-                            url: '/texas_particles-step-1.json'
-                        }),
-                        style: function(feature, resolution){
-                            return [new ol.style.Style({
-                                image: part_icon
-                            })];
-                        }
                     })
                 ],
                 renderer: 'canvas',
@@ -111,6 +98,94 @@ define([
                     zoom: 8
                 })
             });
+    
+            // this.current_source = new ol.source.GeoJSON({
+            //     projection: 'EPSG:3857',
+            //     url: '/COOPSu_NGOFS-step-1.json'
+            // });
+    
+            // this.current_layer = new ol.layer.Vector({
+            //     source: this.current_source,
+            //     style: function(feature, resolution){
+            //         if(resolution < 250){
+            //             return [new ol.style.Style({
+            //                 image: icon_style
+            //             })];
+            //         }
+            //     }
+            // });
+
+            this.spill_source = new ol.source.GeoJSON({
+                projection: 'EPSG:3857',
+                name: 'spill source',
+                url: '/texas_particles-step-1.json'
+            });
+
+            this.spill_layer = new ol.layer.Vector({
+                source: this.spill_source,
+                name: 'spill',
+                style: function(feature, resolution){
+                    return [new ol.style.Style({
+                        image: part_icon_style
+                    })];
+                }
+            });
+
+            this.step[0] = this.spill_layer;
+
+            //webgnome.map.addLayer(this.current_layer);
+            webgnome.map.addLayer(this.step[0]);
+        },
+
+        play: function(){
+            var part_icon_style = this.part_icon_style;
+
+            if(this.index == 48){
+                this.index = -1;
+                return false;
+            } else if (this.index < 48) {
+                this.index += 1;
+            } else {
+                return false;
+            }
+            
+            if(this.step[this.index] === undefined){
+                this.spill_source = new ol.source.GeoJSON({
+                    projection: 'EPSG:3857',
+                    url: '/texas_particles-step-' + (this.index + 1) + '.json'
+                });
+
+                this.spill_layer = new ol.layer.Vector({
+                    source: this.spill_source,
+                    style: function(feature, resolution){
+                        return [new ol.style.Style({
+                            image: part_icon_style
+                        })];
+                    }
+                });
+                this.spill_layer.once('render', function(){
+                    if (this.index !== 0){
+                        this.step[this.index - 1].setVisible(0);
+                    } else {
+                        this.step[48].setVisible(0);
+                    }
+                    this.play();
+                }, this);
+                this.step[this.index] = this.spill_layer;
+                webgnome.map.addLayer(this.spill_layer);
+            } else {
+                this.step[this.index].once('render', function(){
+                    if (this.index !== 0){
+                        this.step[this.index - 1].setVisible(0);
+                    } else {
+                        this.step[48].setVisible(0);
+                    }
+                    this.play();
+                }, this);
+                this.step[this.index].setVisible(1);
+            }
+
+            
         },
 
         close: function(){
