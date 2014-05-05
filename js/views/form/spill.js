ec6df5c693bbc403081585910350b463cca4a043
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-05-05 08:12:06 -0700

    created default map view
    updated all individual uses of ol to default map view

--- js/views/form/spill.js
+++ js/views/form/spill.js
@@ -6,12 +6,12 @@ define([
     'views/default/map',
     'lib/text!templates/form/spill-map.html',
     'lib/text!templates/form/spill-form.html',
-    'lib/ol'
 ], function($, _, Backbone, FormModal, olMapView, MapTemplate, SpillTemplate) {
     var spillForm = FormModal.extend({
         className: 'modal fade form-modal spill-form',
         name: 'spill',
         title: 'Spill',
+        map: new olMapView(),
         
         initialize: function(options, GnomeSpills, GnomeMap) {
             FormModal.prototype.initialize.call(this, options);
@@ -19,7 +19,6 @@ define([
             this.body = _.template(MapTemplate);
             this.GnomeSpills = GnomeSpills;
             this.GnomeMap = GnomeMap;
-            this.map = new olMapView();
 
             this.render();
         },
--- js/views/location/index.js
+++ js/views/location/index.js
@@ -2,35 +2,23 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'lib/ol',
+    'views/default/map',
     'lib/text!templates/location/index.html'
-], function($, _, Backbone, ol, LocationsTemplate){
+], function($, _, Backbone, olMapView, LocationsTemplate){
     var locationsView = Backbone.View.extend({
         className: 'container page locations',
+        map: new olMapView({
+        }),
 
         initialize: function(){
             this.render();
-        },
-
-        setupMap: function(){
-            var map = new ol.Map({
-                target: 'map',
-                layers: [
-                    new ol.layer.Tile({
-                        source: new ol.source.MapQuest({layer: 'osm'})
-                    })
-                ],
-                view: new ol.View2D({
-                    center: ol.proj.transform([-99.6, 40.6], 'EPSG:4326', 'EPSG:3857'),
-                    zoom: 4
-                })
-            });
+            webgnome.map = this.map;
         },
 
         render: function(){
             compiled = _.template(LocationsTemplate);
             $('body').append(this.$el.html(compiled));
-            this.setupMap();
+            this.map.render();
         }
     });
 
