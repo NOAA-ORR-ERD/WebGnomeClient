4efc6039df951ce9fd899b8fb1615ea0dc5e5ca3
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-05-21 17:24:03 -0700

    updating spill and release models spill to support nested objects when parsing release with better defaults

--- js/model/spill.js
+++ js/model/spill.js
@@ -9,11 +9,24 @@ define([
         url: '/spill',
 
         defaults: {
-            'json_': 'webapi',
             'on': true,
             'obj_type': 'gnome.spill.spill.Spill',
             'release': new GnomeRelease(),
             'element_type': new GnomeElement(),
+        },
+
+        model: {
+            release: GnomeRelease,
+            element_type: GnomeElement
+        },
+
+        parse: function(response){
+            for(var key in this.model){
+                var embeddedClass = this.model[key];
+                var embeddedData = response[key];
+                response[key] = new embeddedClass(embeddedData, {parse:true});
+            }
+            return response;
         }
     });
 
