2ae463c5ee5cb0a66e953b0f978277c5b600d1d7
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-05-23 09:54:18 -0700

    updated tests to ensure correct order of operations

--- js/views/tests/spill.js
+++ js/views/tests/spill.js
@@ -1,54 +1,83 @@
 define([
-    'model/spill'
-], function(GnomeSpill){
-    var run = function(){
-        // local spill
-            test("Local spill creation", function(){
-                var spill = new GnomeSpill();
-                ok(spill.cid, "Spill has a client id.");
-                ok(spill.get('release'), "Spill has a release object.");
-                ok(spill.get('element_type'), "Spill has an element_type");
-            });
-
-        // persist spill
-            var persist_spill = new GnomeSpill();
-            var persist_test = function(){
-                test('Persist spill to server', function(){
-                    ok(persist_spill.id, "Spill has py_gnome given id.");
-                    ok(persist_spill.get('release').id, "Spill's release object has a py_gnome given id.");
-                    ok(persist_spill.get('element_type').id, "Spill's element_type object has a py_gnome given id.");
+    'underscore',
+    'model/spill',
+], function(_, GnomeSpill, SessionModel){
+    var spillTest = {
+        run: function() {
+            this.test();
+        },
+        test: function() {
+            // local spill
+                test("Local spill creation", function(){
+                    var spill = new GnomeSpill();
+                    ok(spill.cid, "Spill has a client id.");
+                    ok(spill.get('release'), "Spill has a release object.");
+                    ok(spill.get('element_type'), "Spill has an element_type");
                 });
-            };
-            persist_spill.save(null, {
-                error: persist_test,
-                success: persist_test
-            });
 
-        // update spill
-            var update_spill = new GnomeSpill();
-            var update_test = function(){
-                test('Update spill', function(){
-                    equal(update_spill.id, spill_id, "The same spill was updated.");
-                    equal(update_spill.get('release').id, release_id, "The same release was updated.");
+            // persist spill
+                var persist_spill = new GnomeSpill();
+                var persist_test = function(model, response, options){
+                    test('Persist spill to server', function(){
+                        ok(response.id, "Spill has py_gnome given id.");
+                        ok(response.release.id, "Spill's release object has a py_gnome given id.");
+                        ok(response.release.id, "Spill's element_type object has a py_gnome given id.");
+                    });
+                };
+                persist_spill.save(null, {
+                    error: persist_test,
+                    success: persist_test
                 });
-            };
-            var update_start = function(){
-                var spill_id = update_spill.id;
-                var release_id = update_spill.get('release').id;
 
-                var release = update_spill.get('release');
-                release.set('start_position', [1,1,0]);
+            // get spill
+                var get_test = function(model, response, options){
+                    test('Get spill from server', function(){
+                        ok(response.id, "Spill has py_gnome given id.");
+                    });
+                };
+                var start_get = function(model, response, options) {
+                    model.fetch({
+                        error: get_test,
+                        success: get_test
+                    });
+                };
+                var get_spill = new GnomeSpill();
+                get_spill.save(null, {
+                    error: start_get,
+                    success: start_get
+                });
+                    
+            // update spill
+                var spill_id;
+                var release_id;
+                var update_spill = new GnomeSpill();
+                var update_test = function(model, response, options){
+                    test('Update spill', function(){
+                        equal(response.id, spill_id, "The same spill was updated.");
+                        equal(response.release.id, release_id, "The same release was updated.");
+                        deepEqual(response.release.get('start_position'), [1,1,0], "The release start position was updated");
+                    });
+                };
+                var update_start = function(model, response, options){
+                    model.parse(response);
+                    spill_id = model.id;
+                    release_id = model.get('release').id;
+                    console.log(model);
+                    model.get('release').on('change', _.bind(function(){
+                        this.save(null, {
+                            error: update_test,
+                            success: update_test
+                        });
+                    }, model));
+                    model.get('release').set('start_position', [1,1,0]);
+
+                };
                 update_spill.save(null, {
-                    error: update_test,
-                    success: update_test
+                    error: update_start,
+                    success: update_start
                 });
-            };
-            update_spill.save(null, {
-                error: update_start,
-                success: update_start
-            });
-            
+        }
     };
 
-    return {run: run};
+    return spillTest;
 });
