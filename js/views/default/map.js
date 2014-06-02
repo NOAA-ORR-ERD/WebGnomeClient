d9863b6e5184fa7fd432a49573b4636271268501
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-06-02 09:26:54 -0700

    added overlay support to map view

--- js/views/default/map.js
+++ js/views/default/map.js
@@ -25,6 +25,7 @@ define([
             })
         ],
         center: [-99.6, 40.6],
+        overlays: [],
 
 
         initialize: function(options){
@@ -64,8 +65,11 @@ define([
                 if(!_.isUndefined(options.center)){
                     this.center = options.center;
                 }
-            }
 
+                if(!_.isUndefined(options.center)){
+                    this.overlays = options.overlays;
+                }
+            }
         },
 
         render: function(){
