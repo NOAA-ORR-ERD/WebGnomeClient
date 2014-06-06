1f435c01171e3769c27e17c73244bf01ea766c45
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-06-06 15:00:42 -0700

    updating gnomemodel object

--- js/model/gnome.js
+++ js/model/gnome.js
@@ -2,8 +2,10 @@ define([
     'underscore',
     'backbone',
     'moment',
-    'model/base'
-], function(_, Backbone, moment, BaseModel){
+    'model/base',
+    'model/map',
+    'model/spill'
+], function(_, Backbone, moment, BaseModel, MapModel, SpillModel){
     var gnomeModel = BaseModel.extend({
         url: '/model',
         
@@ -11,19 +13,23 @@ define([
             'outputters': null,
             'obj_type': 'gnome.model.Model',
             'weathering_substeps': null,
-            'map_id': null,
+            'map': new MapModel(),
             'movers': null,
             'start_time': Math.round(Date.now() / 1000),
             'environment': null,
             'cache_enabled': 'false',
             'weatherers': null,
-            'spills': null,
+            'spills': [new SpillModel()],
             'time_step': '900',
             'duration': '86400',
             'uncertain': false,
             'id': null
         },
 
+        model: {
+            map: MapModel,
+            spills: SpillModel
+        },
 
         validate: function(attrs, options) {
             if(attrs.duration <= 0 || isNaN(attrs.duration)){
