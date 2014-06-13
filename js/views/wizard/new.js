bdc4344e9f0f6839665818ebdd625f9438e96299
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-06-13 11:31:26 -0700

    scoped the model of the new wizard globaly

--- js/views/wizard/new.js
+++ js/views/wizard/new.js
@@ -13,23 +13,23 @@ define([
 ], function($, _, Backbone, DefaultWizard, ModelForm, MapForm, SpillForm, EnvironmentForm, GnomeModel, GnomeMap, GnomeSpills){
     var newWizardView = DefaultWizard.extend({
         initialize: function(){
-            this.model = new GnomeModel();
+            webgnome.model = new GnomeModel();
             this.steps = [
                 new ModelForm({
                     name: 'step1',
                     title: 'Model Settings <span class="sub-title">New Model Wizard</span>',
                     buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="next">Next</button>',
-                }, this.model),
+                }, webgnome.model),
                 new MapForm({
                     name: 'step2',
                     title: 'Map <span class="sub-title">New Model Wizard</span>',
                     buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="back">Back</button><button type="button" class="next">Next</button>',
-                }, this.model.get('map')),
+                }, webgnome.model.get('map')),
                 new SpillForm({
                     name: 'step3',
                     title: 'Spill <span class="sub-title">New Model Wizard</span>',
                     buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="back">Back</button><button type="button" class="next">Next</button>',
-                }, this.model.get('spills'), this.model.get('map'))
+                }, webgnome.model.get('spills'), webgnome.model.get('map'))
             ];
             this.start();
         }
