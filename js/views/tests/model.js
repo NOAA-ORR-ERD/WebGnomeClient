a72acce51ef32ae65809bac57b229e3774cf752a
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-06-17 08:32:35 -0700

    appropriate test for the map hydration on the gnome model

--- js/views/tests/model.js
+++ js/views/tests/model.js
@@ -13,8 +13,10 @@ define([
                 model.save(null, {validate: false, success: function(){
                     asyncTest('Gnome Model Creation', function(){
                         ok(model.id, 'Gnome model has an api given id');
-                        equal(model.get('map_id').get('obj_type'), 'gnome.map.GnomeMap', 'Gnome model has a hydrated map object');
-                        start();
+                        model.get('map_id').on('change', function(){
+                            equal(model.get('map_id').get('obj_type'), 'gnome.map.GnomeMap', 'Gnome model has a hydrated map object');
+                            start();
+                        });
                     });
                 }});
         }
