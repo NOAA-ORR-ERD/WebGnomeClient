0fce75d815f250feaf689cdfffe971f361b8d9ac
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-06-17 06:47:38 -0700

    Revert "removing recursive parse init from base class"
    This reverts commit bf6ab85efd84d751365d55e2348f197e7fcaf3b6.

--- js/model/base.js
+++ js/model/base.js
@@ -5,7 +5,22 @@ define([
     var baseModel = Backbone.Model.extend({
         initialize: function(options){
             Backbone.Model.prototype.initialize.call(options, this);
-        },
+
+            for(var key in this.model){
+                var embeddedClass = this.model[key];
+
+                if(_.isNull(this.get(key))){
+                    this.set(key, new embeddedClass());
+                } else if(_.isArray(this.get(key)) && _.isEmpty(this.get(key))){
+                    var collection = new Backbone.Collection();
+                    if(!_.isNull(embeddedClass)){
+                        collection.add(new embeddedClass());
+                    }
+                    
+                    this.set(key, collection);
+                }
+            }
+        }
     });
 
     return baseModel;
