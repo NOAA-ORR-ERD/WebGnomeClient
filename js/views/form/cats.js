a8b42922be927773a2fd692f866db805b7db689d
Alex Miranda <alex.miranda@noaa.gov>
2014-07-25 12:58:51 -0700

    Added more content to the cats mover modal's template

--- js/views/form/cats.js
+++ js/views/form/cats.js
@@ -6,11 +6,12 @@ define([
     'text!templates/form/cats.html'
 ], function($, _, Backbone, FormModal, FormTemplate){
 	var catsForm = FormModal.extend({
-		title: 'Cats Mover',
+		title: 'Current Mover Settings',
 		className: 'modal fade form-modal',
 
-		initialize: function(options, modal){
+		initialize: function(options, GnomeTide){
 			FormModal.prototype.initialize.call(this, options);
+			this.model = GnomeTide;
 		},
 
 		render: function(options){
