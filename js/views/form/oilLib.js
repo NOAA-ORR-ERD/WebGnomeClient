c96712d56e7e729614e582cd1289a90469d71f70
Alex Miranda <alex.miranda@noaa.gov>
2014-08-08 15:22:32 -0700

    View changes to fix tooltip bug

--- js/views/form/oilLib.js
+++ js/views/form/oilLib.js
@@ -29,21 +29,27 @@ define([
 
             // Use the jquery-ui slider to enable a slider so the user can select the range of API
             // values they would want to search for
+            this.createSliders();
+        },
 
+        createSliders: function(){
             this.$('.slider').slider({
                         range: true, 
                         min: -2, 
                         max: 180,
                         values: [-2,180],
-                        // create: function(e, ui){
-                        //    $('.ui-slider-handle:first').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + ui.values[0] + '</div></div>');
-                        //    $('.ui-slider-handle:last').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + ui.values[1] + '</div></div>');
-                        // },
+                        create: function(e, ui){
+                           $('.ui-slider-handle:first').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + 
+                                                             ui.values[0] + '</div></div>');
+                           $('.ui-slider-handle:last').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + 
+                                                             ui.values[1] + '</div></div>');
+                        },
                         slide: function(e, ui){
                            $('.ui-slider-handle:first').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + 
                                                              ui.values[0] + '</div></div>');
                            $('.ui-slider-handle:last').html('<div class="tooltip top slider-tip"><div class="tooltip-arrow"></div><div class="tooltip-inner">' + 
                                                              ui.values[1] + '</div></div>');
+                           console.log(ui);
                         }
                     });
         }
