dfb02fe2f7a34f84a80f882dad1489567a073c1e
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-08-15 14:26:58 -0700

    started spill form refactor.

--- js/views/form/spill.js
+++ js/views/form/spill.js
@@ -5,314 +5,85 @@ define([
     'moment',
     'ol',
     'views/modal/form',
-    'views/default/map',
-    'text!templates/form/spill-map.html',
     'text!templates/form/spill-form.html',
-], function($, _, Backbone, moment, ol, FormModal, olMapView, MapTemplate, SpillTemplate) {
+], function($, _, Backbone, moment, ol, FormModal, SpillTemplate) {
     var spillForm = FormModal.extend({
         className: 'modal fade form-modal spill-form',
         name: 'spill',
         title: 'Spill',
-        interaction: null,
 
-        events: function(){
-            return _.defaults({
-                'click .point': 'newPoint',
-                'click .line': 'newLine',
-            }, FormModal.prototype.events);
-        },
-        
-        initialize: function(options, GnomeSpills, GnomeMap) {
+        initialize: function(options, model) {
             FormModal.prototype.initialize.call(this, options);
 
-            this.body = _.template(MapTemplate);
-            this.GnomeSpills = GnomeSpills;
-            this.GnomeMap = GnomeMap;
-            this.source = new ol.source.Vector();
+            this.model = (model ? model : null);
+        },
 
-            this.GnomeSpills.forEach(_.bind(function(spill){
-                if (_.difference(spill.get('release').get('start_position'), spill.get('release').get('end_position')).length === 0) {
-                    var coords = ol.proj.transform(spill.get('release').get('start_position'), 'EPSG:4326', 'EPSG:3857');
-                    var feature = new ol.Feature(new ol.geom.Point(coords));
-                    feature.set('cid', spill.cid);
-                    this.source.addFeature(feature);
-                } else {
-                    var start_coords = ol.proj.transform(spill.get('release').get('start_position'), 'EPSG:4326', 'EPSG:3857');
-                    var end_coords = ol.proj.transform(spill.get('release').get('end_position'), 'EPSG:4326', 'EPSG:3857');
-                    var feature = new ol.Feature(new ol.geom.LineString([start_coords, end_coords]));
-                    feature.set('cid', spill.cid);
-                    this.source.addFeature(feature);
-                }
-            }, this));
-
-
-            this.vector = new ol.layer.Vector({
-                source: this.source,
-                name: 'spills'
-            });
-            
-            this.select = new ol.interaction.Select({
-                layers:[
-                    this.vector
-                ],
-                style: [new ol.style.Style({
-                    image: new ol.style.Circle({
-                        radius: 8,
-                        fill: null,
-                        stroke: new ol.style.Stroke({color: 'orange', width: 2})
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: 'orange',
-                        width: 2
-                    })
-                })]
+        render: function(options){
+            var spill = this.model;
+            this.body = _.template(SpillTemplate, {
+                start_lat: spill.get('release').get('start_position')[1],
+                start_lng: spill.get('release').get('start_position')[0],
+                end_lat: spill.get('release').get('end_position')[1],
+                end_lng: spill.get('release').get('end_position')[0],
+                name: spill.get('name'),
+                release_amount: spill.get('release-amount'),
+                release_start: _.isNull(spill.get('release').get('release_time')) ? moment().format('YYYY/M/D H:mm') : moment(spill.get('release').get('release_time')).format('YYYY/M/D H:mm'),
+                release_end: _.isNull(spill.get('release').get('end_release_time')) ? moment().format('YYYY/M/D H:mm') : moment(spill.get('release').get('end_release_time')).format('YYYY/M/D H:mm')
             });
 
-            this.select.getFeatures().on('add', function(event){
-                var feature = event.element;
-                this.loadSpill(feature);
-            }, this);
+            FormModal.prototype.render.call(this, options);
 
-            this.select.getFeatures().on('remove', function(event){
-                this.unloadSpill(event);
-            }, this);
-
-            this.modify = new ol.interaction.Modify({
-                features: this.select.getFeatures()
+            this.$('#release-start').datetimepicker({
+                format: 'Y/n/j G:i',
+            });
+            this.$('#release-end').datetimepicker({
+                format: 'Y/n/j G:i',
             });
 
-            this.newPointDraw = new ol.interaction.Draw({
-                source: this.source,
-                type: /** @type {ol.geom.GeometryType} */ ('Point')
-            });
-            this.newLineDraw = new ol.interaction.Draw({
-                source: this.source,
-                type: /** @type {ol.geom.GeometryType} */ ('LineString')
-            });
-
-            if(this.GnomeMap.get('filename') == 'EmptyMap.bna'){
-                this.ol = new olMapView({
-                    id: 'spill-form-map',
-                    interactions: ol.interaction.defaults().extend([
-                        this.select
-                    ]),
-                    layers: [
-                        this.vector
-                    ]
-                });
+            // set the correct select input on the forms
+            if(!_.isUndefined(spill.get('pollutant'))){
+                this.$('#pollutant').find('option[value="' + spill.get('pollutant') + '"]').attr('selected', 'selected');
             }
-        },
-
-        ready: function() {
-            this.ol.render();
-
-            // hover listener for map
-            this.$(this.ol.map.getViewport()).on('mousemove', _.bind(function(event){
-                var pixel = this.ol.map.getEventPixel(event.originalEvent);
-                var feature = this.ol.map.forEachFeatureAtPixel(pixel, function(feature, layer){
-                    return feature;
-                });
-
-                if (feature) {
-                    this.ol.map.getViewport().style.cursor = 'pointer';
-                } else {
-                    this.ol.map.getViewport().style.cursor = '';
-                }
-            }, this));
-        },
-
-        newPoint: function() {
-            this.ol.map.removeInteraction(this.interaction);
-            this.interaction = this.newPointDraw;
-            this.ol.map.addInteraction(this.interaction);
-            this.interaction.on('drawend', this.drawEnd, this);
-        },
-
-        newLine: function() {
-            this.ol.map.removeInteraction(this.interaction);
-            this.interaction = this.newLineDraw;
-            this.ol.map.addInteraction(this.interaction);
-            this.interaction.on('drawend', this.drawEnd, this);
-        },
-
-        drawEnd: function(event) {
-            this.ol.map.removeInteraction(this.interaction);
-            this.interaction = null;
-            var feature = event.feature;
-
-            // double check that a feature is selected if not load it
-            setTimeout(_.bind(function(){
-                if (this.select.getFeatures().getLength() === 0){
-                    this.select.getFeatures().push(feature);
-                }
-            }, this), 400);
-
-            // create a new spill model in the spills collection and add the cid
-            // to the feature
-            var spill = this.GnomeSpills.add({'on': true});
-            spill.get('release').set({
-                start_position: ol.proj.transform(feature.getGeometry().getFirstCoordinate(), 'EPSG:3857', 'EPSG:4326'),
-                end_position: ol.proj.transform(feature.getGeometry().getLastCoordinate(), 'EPSG:3857', 'EPSG:4326')
-            });
-            spill.set('name', 'New Spill ' + spill.cid);
-            feature.set('cid', spill.cid);
-        },
-
-        loadSpill: function(feature){
-            if (this.$('.spill-form').length === 0){
-                // disable the modals buttons until the spill is finished being edited.
-                this.$('.modal-footer button').addClass('disabled');
-
-                var spill = this.GnomeSpills.get(feature.get('cid'));
-                this.$('.map').css('height', '200px');
-                this.ol.map.updateSize();
-                this.$('.modal-body').append(_.template(SpillTemplate, {
-                    start_lat: spill.get('release').get('start_position')[1],
-                    start_lng: spill.get('release').get('start_position')[0],
-                    end_lat: spill.get('release').get('end_position')[1],
-                    end_lng: spill.get('release').get('end_position')[0],
-                    name: spill.get('name'),
-                    release_amount: spill.get('release-amount'),
-                    release_start: _.isNull(spill.get('release').get('release_time')) ? moment().format('YYYY/M/D H:mm') : moment(spill.get('release').get('release_time')).format('YYYY/M/D H:mm'),
-                    release_end: _.isNull(spill.get('release').get('end_release_time')) ? moment().format('YYYY/M/D H:mm') : moment(spill.get('release').get('end_release_time')).format('YYYY/M/D H:mm')
-                }));
-                this.$('#release-start').datetimepicker({
-                    format: 'Y/n/j G:i',
-                });
-                this.$('#release-end').datetimepicker({
-                    format: 'Y/n/j G:i',
-                });
-
-                // the feature is a point geometry hide the end position inputs 
-                if (feature.getGeometry().getType() === 'Point') {
-                    this.$('.end-position').hide();
-                }
-
-                this.ol.map.getView().setCenter(feature.getGeometry().getFirstCoordinate());
-
-                // set the correct select input on the forms
-                if(!_.isUndefined(spill.get('pollutant'))){
-                    this.$('#pollutant').find('option[value="' + spill.get('pollutant') + '"]').attr('selected', 'selected');
-                }
-                if(!_.isUndefined(spill.get('release-unit'))){
-                    this.$('#release-unit').find('option[value="' + spill.get('release-unit') + '"]').attr('selected', 'selected');
-                }
-
-                // setup a drag listener that will keep the selected point updated
-                // if the user moves it on the map
-                // for when using the modify control on the map, currently not supported.
-                // this.dragkey = this.ol.map.on('pointerdrag', _.bind(this.mapUpdateSpill, this));
-
-                this.$('.remove').on('click', _.bind(function(e){
-                    e.preventDefault();
-                    this.removeSpill();
-                }, this));
-
-                this.update();
-            }
-        },
-
-        unloadSpill: function(event){
-            var spill = this.GnomeSpills.get(event.element.get('cid'));
-            var release = spill.get('release');
-            if(release.get('start_position').length < 3){
-                release.get('start_position').push(0);
-            }
-            if(release.get('end_position').length < 3){
-                release.get('end_position').push(0);
-            }
-
-            if(spill.save()){
-                this.$('.map').css('height', '');
-                this.ol.map.updateSize();
-                this.$('.spill-form').remove();
-                $('.xdsoft_datetimepicker').remove();
-                //this.ol.map.unByKey(this.dragkey);
-
-                // enable the modals buttons until the spill is finished being edited.
-                this.$('.modal-footer button').removeClass('disabled');
+            if(!_.isUndefined(spill.get('release-unit'))){
+                this.$('#release-unit').find('option[value="' + spill.get('release-unit') + '"]').attr('selected', 'selected');
             }
         },
 
         update: function(){
-            // when information on the form is updated update the spill object
-            // and the map feature.
-            var feature = this.select.getFeatures().getArray()[0];
-            if(feature){
-                var spill = this.GnomeSpills.get(feature.get('cid'));
-                var release = spill.get('release');
+            var spill = this.spill;
+            var release = spill.get('release');
 
-                spill.set('name', this.$('#name').val());
+            spill.set('name', this.$('#name').val());
 
-                // if this is a point update the end position to be the same as the start position
-                if(feature.getGeometry().getType() === 'Point'){
-                    release.set('start_position', [parseFloat(this.$('#start-lng').val()), parseFloat(this.$('#start-lat').val())]);
-                    release.set('end_position', [parseFloat(this.$('#start-lng').val()), parseFloat(this.$('#start-lat').val())]);
-                    this.$('#end-lng').val(spill.get('release').get('start_position')[0]);
-                    this.$('#end-lat').val(spill.get('release').get('start_position')[1]);
-                } else {
-                    release.set('start_position', [parseFloat(this.$('#start-lng').val()), parseFloat(this.$('#start-lat').val())]);
-                    release.set('end_position', [parseFloat(this.$('#end-lng').val()), parseFloat(this.$('#end-lat').val())]);
-                }
-                
-                spill.set('pollutant', this.$('#pollutant').val());
-                spill.set('release-amount', this.$('#release-amount').val());
-                spill.set('release-unit', this.$('#release-unit').val());
-                var release_start = $('#release-start').val();
-                var release_end = $('#release-end').val();
-                release.set('release_time', moment(release_start, 'YYYY/M/D H:mm').format());
-                release.set('end_release_time', moment(release_end, 'YYYY/M/D H:mm').format());
+            // if this is a point update the end position to be the same as the start position
+            release.set('start_position', [parseFloat(this.$('#start-lng').val()), parseFloat(this.$('#start-lat').val())]);
+            release.set('end_position', [parseFloat(this.$('#start-lng').val()), parseFloat(this.$('#start-lat').val())]);
+            this.$('#end-lng').val(spill.get('release').get('start_position')[0]);
+            this.$('#end-lat').val(spill.get('release').get('start_position')[1]);
+            
+            spill.set('pollutant', this.$('#pollutant').val());
+            spill.set('release-amount', this.$('#release-amount').val());
+            spill.set('release-unit', this.$('#release-unit').val());
+            var release_start = $('#release-start').val();
+            var release_end = $('#release-end').val();
+            release.set('release_time', moment(release_start, 'YYYY/M/D H:mm').format());
+            release.set('end_release_time', moment(release_end, 'YYYY/M/D H:mm').format());
 
-                // update the features lat lng on the map to match the one in the map
-                var start_coords = ol.proj.transform(spill.get('release').get('start_position'), 'EPSG:4326', 'EPSG:3857');
-                var end_coords = ol.proj.transform(spill.get('release').get('end_position'), 'EPSG:4326', 'EPSG:3857');
-
-                this.ol.map.getView().setCenter(start_coords);
-
-                if(feature.getGeometry().getType() === 'Point'){
-                    feature.getGeometry().setCoordinates(start_coords);
-                } else {
-                    var line = feature.getGeometry().getCoordinates();
-                    line[0] = start_coords;
-                    line[1] = end_coords;
-                    feature.getGeometry().setCoordinates(line);
-                }
-
-                if(spill.isValid()){
-                    this.error('Error!', spill.validationError);
-                } else {
-                    this.clearError();
-                }
+            if(!spill.isValid()){
+                this.error('Error!', spill.validationError);
+            } else {
+                this.clearError();
             }
         },
 
-        mapUpdateSpill: function(event) {
-            // update the lat lon values of the spill object and form when the user
-            // modifies it on the map
-            var feature = this.select.getFeatures().getArray()[0];
-            var spill = this.GnomeSpills.get(feature.get('cid'));
-            var start_position = ol.proj.transform(feature.getGeometry().getFirstCoordinate(), 'EPSG:3857', 'EPSG:4326');
-            var end_point = ol.proj.transform(feature.getGeometry().getLastCoordinate(), 'EPSG:3857', 'EPSG:4326');
-
-            spill.get('release').set('start_position', start_position);
-            spill.set('end_point', end_point);
-
-            this.$('#lng').val(spill.get('release').get('start_position')[0]);
-            this.$('#lat').val(spill.get('release').get('start_position')[1]);
-        },
-
-        removeSpill: function(){
-            var feature = this.select.getFeatures().getArray()[0];
-            var spill = this.GnomeSpills.get(feature.get('cid'));
-            this.GnomeSpills.remove(spill);
-            this.select.getFeatures().clear();
-            this.source.removeFeature(feature);
+        save: function(){
+            FormModal.prototype.save.call(this, function(){
+                $('.xdsoft_datetimepicker').remove();
+            });
         },
 
         close: function(){
-            if(this.ol){
-                this.ol.close();
-            }
+            $('.xdsoft_datetimepicker').remove();
             FormModal.prototype.close.call(this);
         }
     });
