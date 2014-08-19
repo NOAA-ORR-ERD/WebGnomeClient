d53b9dad22fd18cd3f3687c5359f81290aa23d7c
Alexander Miranda <alex.miranda@noaa.gov>
2014-08-19 14:15:48 -0700

    Added oilLib model to application

---
+++ js/model/resources/oilLib.js
@@ -0,0 +1,30 @@
+define([
+    'underscore',
+    'jquery',
+    'backbone'
+], function(_, $, Backbone){
+    var oilLib = Backbone.Collection.extend({
+        initialize: function(cb){
+            this.fetch({
+                success: cb
+            });
+        },
+        url: function(){
+            return 'http://0.0.0.0:9898/oil';
+        },
+
+        fetch: function(options){
+        
+            if(_.isUndefined(options)){
+                options = {};
+            }
+            if(!_.has(options, 'data')){
+                options.data = {};
+            }
+            Backbone.Model.prototype.fetch.call(this, options);
+        }
+
+    });
+
+    return oilLib;
+});
