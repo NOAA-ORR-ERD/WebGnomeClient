13122fe90d18b6447746f8ebfb65b48314b9b4de
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-08-22 10:40:28 -0700

    reduced the default number of elements on a release

--- js/model/release.js
+++ js/model/release.js
@@ -11,7 +11,7 @@ define([
             'obj_type': 'gnome.spill.release.PointLineRelease',
             'end_position': [0, 0, 0],
             'start_position': [0, 0, 0],
-            'num_elements': 1000,
+            'num_elements': 500,
             'num_released': 0,
             'start_time_invalid': true,
             'release_time': '2013-02-13T15:00:00',
