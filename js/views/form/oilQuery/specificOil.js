8e6d8e7ec3029c04560c6146cd155095207a9dd8
Alexander Miranda <alex.miranda@noaa.gov>
2014-09-03 11:24:12 -0700

    Template changes to specific oil view

--- js/views/form/oilQuery/specificOil.js
+++ js/views/form/oilQuery/specificOil.js
@@ -13,14 +13,15 @@ define([
 		},
 
 		render: function(){
-			var data = this.model.attributes;
+			var data = this.dataParse(this.model.attributes);
 			var compiled = _.template(SpecificOilTemplate, {data: data});
 			$('.modal-body').html(compiled);
 		},
 
 		dataParse: function(obj){
 			for (key in obj){
-				key = key.replace(/(^| )(\w)/g, String.toUpperCase);
+				if (!obj[key]){
+					obj[key] = "N/A";				}
 			}
 			return obj;
 		}
