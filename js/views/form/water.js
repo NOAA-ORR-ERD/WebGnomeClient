ff96931bbd1bc37af6bd897eb911dc549761b6fa
Alexander Miranda <alex.miranda@noaa.gov>
2014-09-11 15:39:08 -0700

    Enabled the user to toggle the manual input fields through selecting the other option for either sediment load and/or salinity

--- js/views/form/water.js
+++ js/views/form/water.js
@@ -4,13 +4,17 @@ define([
     'backbone',
     'views/modal/form',
     'text!templates/form/water.html',
-    'jqueryDatetimepicker',
-    'compassui'
+    'jqueryDatetimepicker'
 ], function($, _, Backbone, FormModal, WaterTemplate){
     var waterForm = FormModal.extend({
     	className: 'modal fade form-modal model-form',
     	title: 'Water',
-    	size: 'lg',
+
+    	events: function(){
+    		return _.defaults({
+    			'change select': 'revealManualInputs'
+    		}, FormModal.prototype.events);
+    	},
 
     	initialize: function(options, model){
     		FormModal.prototype.initialize.call(this, options);
@@ -22,10 +26,24 @@ define([
 
     		FormModal.prototype.render.call(this, options);
 
+    		this.$('#sediment-entry').hide();
+    		this.$('#salinity-entry').hide();
+
     	},
 
     	update: function(){
 
+    	},
+
+    	revealManualInputs: function(e){
+    		var value = e.currentTarget.value;
+    		var id = e.currentTarget.id;
+    		if (value === 'other' && id !== this.selectedId){
+    			this.$('#' + id + '-entry').show();
+    		} else {
+    			this.$('#' + id + '-entry').hide();
+    		}
+    		this.selectId = id;
     	}
 
     });
