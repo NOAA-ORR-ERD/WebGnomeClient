cf9a8f003513b110f9e738232794a1e067671a1c
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-09-11 14:34:52 -0700

    single quotes not double.

--- js/views/model/setup.js
+++ js/views/model/setup.js
@@ -164,12 +164,12 @@ define([
                         datasets: [
                             {
                                 label: 'Wind Speed',
-                                fillColor: "rgba(151,187,205,0.2)",
-                                strokeColor: "rgba(151,187,205,1)",
-                                pointColor: "rgba(151,187,205,1)",
-                                pointStrokeColor: "#fff",
-                                pointHighlightFill: "#fff",
-                                pointHighlightStroke: "rgba(151,187,205,1)",
+                                fillColor: 'rgba(151,187,205,0.2)',
+                                strokeColor: 'rgba(151,187,205,1)',
+                                pointColor: 'rgba(151,187,205,1)',
+                                pointStrokeColor: '#fff',
+                                pointHighlightFill: '#fff',
+                                pointHighlightStroke: 'rgba(151,187,205,1)',
                                 data: data
                             }
                         ]
