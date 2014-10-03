c3b3b8b637ede43a6784913d1139178d336073d7
Alexander Miranda <alex.miranda@noaa.gov>
2014-10-03 08:59:51 -0700

    Corrected selector misspelling for the wave_height input field selector

--- js/views/form/water.js
+++ js/views/form/water.js
@@ -34,7 +34,8 @@ define([
             if (this.model.get('fetch')){
                 this.$('#data-source').val('fetch');
                 this.$('#fetch').val(this.model.get('fetch'));
-            } else if (this.model.get('wave_height')){
+            }
+            if (this.model.get('wave_height')){
                 this.$('#data-source').val('specified');
                 this.$('#height').val(this.model.get('wave_height'));
             }
@@ -70,7 +71,6 @@ define([
 
         triggerInputs: function(){
             this.$('#data-source').trigger('change');
-            console.log('trigger ran');
         },
 
         convertHeighttoKM: function(val, unit){
@@ -87,14 +87,14 @@ define([
             this.model.set('wave_height', 0);
             if (this.$('#data-source').val() === 'fetch'){
                 this.model.set('fetch', this.$('#fetch').val());
-            } else if (this.$('#data-source').val() === 'specified'){
-                this.model.set('wave_height', this.$('height').val());
+            }
+            if (this.$('#data-source').val() === 'specified'){
+                this.model.set('wave_height', this.$('#height').val());
             }
             this.model.set('units', units);
             this.model.set('temperature', this.$('#temp').val());
             this.model.set('salinity', this.$('.salinity:visible').val());
             this.model.set('sediment', this.$('.sediment:visible').val());
-            console.log(this.model);
             if(!this.model.isValid()){
                 this.error('Error!', this.model.validationError);
             } else {
