530e15f6e0fc9a9fd258e5f404e5f1a54d948fb5
Alexander Miranda <alex.miranda@noaa.gov>
2014-10-07 08:37:39 -0700

    Exported all map rendering to it's own method to remove duplicate code in manualMapInput and locationSelect

--- js/views/form/spill/base.js
+++ js/views/form/spill/base.js
@@ -85,90 +85,7 @@ define([
 			oilLibraryView.on('hidden', _.bind(this.show, this));
 		},
 
-		locationSelect: function(e, pastCoords){
-			if (!this.mapShown){
-                this.$('.map').show();
-                this.source = new ol.source.Vector();
-                this.layer = new ol.layer.Vector({
-                    source: this.source,
-                    style: new ol.style.Style({
-                        image: new ol.style.Icon({
-                            anchor: [0.5, 1.0],
-                            src: '/img/spill-pin.png',
-                            size: [32, 40]
-                        })
-                    })
-                });
-
-                var startPosition = _.initial(this.model.get('release').get('start_position'));
-                this.spillMapView = new SpillMapView({
-                    id: 'spill-form-map',
-                    zoom: 2,
-                    center: [-128.6, 42.7],
-                    layers: [
-                        new ol.layer.Tile({
-                            source: new ol.source.MapQuest({layer: 'osm'})
-                        }),
-                        this.layer
-                    ]
-                });
-				this.spillMapView.render();
-                var map = webgnome.model.get('map');
-                if (!_.isUndefined(map) && map.get('obj_type') !== 'gnome.map.GnomeMap'){
-                    map.getGeoJSON(_.bind(function(data){
-                        this.shorelineSource = new ol.source.GeoJSON({
-                            object: data,
-                            projection: 'EPSG:3857'
-                        });
-                        var extent = this.shorelineSource.getExtent();
-                        this.shorelineLayer = new ol.layer.Vector({
-                            source: this.shorelineSource,
-                            style: new ol.style.Style({
-                                fill: new ol.style.Fill({
-                                    color: [228, 195, 140, 0.6]
-                                }),
-                                stroke: new ol.style.Stroke({
-                                    color: [228, 195, 140, 0.75],
-                                    width: 1
-                                })
-                            })
-                        });
-                        if(this.spillMapView.map){
-                            this.spillMapView.map.getLayers().insertAt(1, this.shorelineLayer);
-                            if (startPosition[0] === 0 && startPosition[1] === 0){
-                                this.spillMapView.map.getView().fitExtent(extent, this.spillMapView.map.getSize());
-                            }
-                        }
-
-                    }, this));
-                }
-                if (startPosition[0] !== 0 && startPosition[1] !== 0){
-                    startPosition = ol.proj.transform(startPosition, 'EPSG:4326', 'EPSG:3857');
-                    var feature = new ol.Feature(new ol.geom.Point(startPosition));
-                    this.source.addFeature(feature);
-                    
-                    this.spillMapView.map.getView().setCenter(startPosition);
-                    this.spillMapView.map.getView().setZoom(15);
-                }
-				this.spillMapView.map.on('click', _.bind(function(e){
-					this.source.forEachFeature(function(feature){
-						this.source.removeFeature(feature);
-					}, this);
-					var feature = new ol.Feature(new ol.geom.Point(e.coordinate));
-					var coords = new ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
-					this.spillCoords = {lat: coords[1], lon: coords[0]};
-                    this.source.addFeature(feature);
-				}, this));
-                setTimeout(_.bind(function(){
-                    this.spillMapView.map.updateSize();
-                }, this), 250);
-			} else {
-                 
-            }
-            this.mapShown = true;
-		},
-
-        manualMapInput: function(){
+        mapRender: function(){
             if (!this.mapShown){
                 this.$('.map').show();
                 this.source = new ol.source.Vector();
@@ -195,7 +112,77 @@ define([
                 });
                 this.spillMapView.render();
                 this.mapShown = true;
+                this.spillMapView.map.on('click', _.bind(function(e){
+                    this.source.forEachFeature(function(feature){
+                        this.source.removeFeature(feature);
+                    }, this);
+                    var feature = new ol.Feature(new ol.geom.Point(e.coordinate));
+                    var coords = new ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
+                    this.spillCoords = {lat: coords[1], lon: coords[0]};
+                    this.source.addFeature(feature);
+                }, this));
+                setTimeout(_.bind(function(){
+                    this.spillMapView.map.updateSize();
+                }, this), 250);
             }
+        },
+
+		locationSelect: function(e, pastCoords){
+            this.mapRender();
+            var map = webgnome.model.get('map');
+            if (!_.isUndefined(map) && map.get('obj_type') !== 'gnome.map.GnomeMap'){
+                map.getGeoJSON(_.bind(function(data){
+                    this.shorelineSource = new ol.source.GeoJSON({
+                        object: data,
+                        projection: 'EPSG:3857'
+                    });
+                    var extent = this.shorelineSource.getExtent();
+                    this.shorelineLayer = new ol.layer.Vector({
+                        source: this.shorelineSource,
+                        style: new ol.style.Style({
+                            fill: new ol.style.Fill({
+                                color: [228, 195, 140, 0.6]
+                            }),
+                            stroke: new ol.style.Stroke({
+                                color: [228, 195, 140, 0.75],
+                                width: 1
+                            })
+                        })
+                    });
+                    if(this.spillMapView.map){
+                        this.spillMapView.map.getLayers().insertAt(1, this.shorelineLayer);
+                        if (startPosition[0] === 0 && startPosition[1] === 0){
+                            this.spillMapView.map.getView().fitExtent(extent, this.spillMapView.map.getSize());
+                        }
+                    }
+
+                }, this));
+            }
+            var startPosition = _.initial(this.model.get('release').get('start_position'));
+            if (startPosition[0] !== 0 && startPosition[1] !== 0){
+                startPosition = ol.proj.transform(startPosition, 'EPSG:4326', 'EPSG:3857');
+                var feature = new ol.Feature(new ol.geom.Point(startPosition));
+                this.source.addFeature(feature);
+                
+                this.spillMapView.map.getView().setCenter(startPosition);
+                this.spillMapView.map.getView().setZoom(15);
+            }
+			this.spillMapView.map.on('click', _.bind(function(e){
+				this.source.forEachFeature(function(feature){
+					this.source.removeFeature(feature);
+				}, this);
+				var feature = new ol.Feature(new ol.geom.Point(e.coordinate));
+				var coords = new ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
+				this.spillCoords = {lat: coords[1], lon: coords[0]};
+                this.source.addFeature(feature);
+			}, this));
+            setTimeout(_.bind(function(){
+                this.spillMapView.map.updateSize();
+            }, this), 250);
+		},
+
+        manualMapInput: function(){
+            this.mapRender();
             this.source.forEachFeature(function(feature){
                         this.source.removeFeature(feature);
                     }, this);
