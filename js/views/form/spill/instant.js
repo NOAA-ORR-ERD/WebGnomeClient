31c4d2c0e7fd5b49d107483f2c541b4d9b065b9d
Alexander Miranda <alex.miranda@noaa.gov>
2014-10-21 07:37:29 -0700

    Updated instant spill view to round uncertainity ranges to the nearest integer amount

--- js/views/form/spill/instant.js
+++ js/views/form/spill/instant.js
@@ -112,11 +112,11 @@ define([
                 if(value === 0){
                     this.$('.tooltip-inner').text(amount);
                 } else {
-                    var bottom = (amount * (1 - ((value / 100.0) * 5))).toFixed(1);
+                    var bottom = parseInt(Math.round((amount * (1 - ((value / 100.0) * 5)))), 10);
                     if (bottom < 0) {
                         bottom = 0;
                     }
-                    var top = (amount * (1 + ((value / 100.0) * 5))).toFixed(1);
+                    var top = parseInt(Math.round((amount * (1 + ((value / 100.0) * 5)))), 10);
                     this.$('.tooltip-inner').text(bottom + ' - ' + top);
                 }
             }
