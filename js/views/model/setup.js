0eacf5f2a2426b90844e3bd65fe69edc6733a7aa
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-12-01 08:02:47 -0800

    fix for stage 3 rendering after just updating the model before selecting a prediction.

--- js/views/model/setup.js
+++ js/views/model/setup.js
@@ -272,7 +272,7 @@ define([
                     container: 'body',
                     delay: delay
                 });
-                if(this.$('.stage-2 .panel:visible').length == this.$('.stage-2 .panel.complete:visible').length){
+                if(this.$('.stage-2 .panel:visible').length == this.$('.stage-2 .panel.complete:visible').length && localStorage.getItem('prediction') !== 'null'){
                     this.$('.stage-3').show();
                     this.updateResponse();
                 } else {
