58e84cfb46b736aade917d5d6944f4d71b101d4d
Alexander Miranda <alex.miranda@noaa.gov>
2014-12-08 14:57:40 -0800

    Updated substance model defaults to have all of the necessary initial information so that oil info can be properly rendered

--- js/model/substance.js
+++ js/model/substance.js
@@ -5,7 +5,10 @@ define([
 ], function(_, Backbone, BaseModel){
     var gnomeSubstance = BaseModel.extend({
         defaults: {
-            name: 'ALAMO'
+            name: 'ALAMO',
+            api: 23.3,
+            pour_point_max_k: 266.4833,
+            flash_point_max_k: 339.97349999999994
         }
     });
     return gnomeSubstance;
