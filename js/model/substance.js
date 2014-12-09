e1111031ebcae75469bd6aaa0da66efb027ecf2b
Alexander Miranda <alex.miranda@noaa.gov>
2014-12-09 09:41:59 -0800

    The oil info now properly displays the oil categories

--- js/model/substance.js
+++ js/model/substance.js
@@ -12,7 +12,8 @@ define([
             name: 'ALAMO',
             api: 23.3,
             pour_point_max_k: 266.4833,
-            flash_point_max_k: 339.97349999999994
+            flash_point_max_k: 339.97349999999994,
+            categories: [{id: 6, name: "Medium", parent_id: 1, children: [], parent: {id: 1, name: "Crude", parent_id: null}}]
         }
     });
     return gnomeSubstance;
