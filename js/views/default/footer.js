210dc4af94888618281854d109833f1a6eaed705
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-12-09 09:00:29 -0800

    added diclaimer and privacy policy modals, added help I'm struck functionality that just reloads the page for now.

--- js/views/default/footer.js
+++ js/views/default/footer.js
@@ -2,11 +2,17 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/default/footer.html'
-], function($, _, Backbone, FooterTemplate){
+    'text!templates/default/footer.html',
+    'views/modal/base'
+], function($, _, Backbone, FooterTemplate, BaseModal){
     var footerView = Backbone.View.extend({
         className: 'footer',
         rendered: false,
+
+        events: {
+            'click .disclaimer': 'disclaimer',
+            'click .privacy': 'privacy'
+        },
 
         initialize: function(){
             if($('body > .footer').length < 1){
@@ -19,6 +25,26 @@ define([
             var compiled = _.template(FooterTemplate);
             this.$el.append(compiled);
             $('body').append(this.$el);
+        },
+
+        disclaimer: function(e){
+            e.preventDefault();
+            var modal = new BaseModal({
+                title: 'Disclaimer',
+                body: 'Disclaimer here!',
+                buttons: '<a href="" class="btn btn-primary" data-dismiss="modal">Ok</a>'
+            });
+            modal.render();
+        }, 
+
+        privacy: function(e){
+            e.preventDefault();
+            var modal = new BaseModal({
+                title: 'Privacy Policy',
+                body: 'Privacy Policy Here!',
+                buttons: '<a href="" class="btn btn-primary" data-dismiss="modal">Ok</a>'
+            });
+            modal.render(); 
         }
     });
 
