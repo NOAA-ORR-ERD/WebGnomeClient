f33975efbbbcf429f1f832184ee70c9127bedd61
Nathan Wilkins <nathan.wilkins@noaa.gov>
2014-12-23 15:04:16 -0800

    fix for when a user clicks the glyphicon specifically inside of a button

--- js/views/model/fate.js
+++ js/views/model/fate.js
@@ -359,6 +359,9 @@ define([
         downloadTableOilBudget: function(e){
             var table = this.$('#budget-table table');
             var type = $(e.target).data('type');
+            if(type == undefined){
+                type = $(e.target).parent().data('type');
+            }
             var name = webgnome.model.get('name') ? webgnome.model.get('name') + ' Oil Budget Table Export' : 'Oil Budget Table Export';
             var filename = name + '.' + type;
             var content = '';
@@ -707,6 +710,9 @@ define([
         downloadTableICS: function(e){
             var table = this.$('#ics209 table:last');
             var type = $(e.target).data('type');
+            if (type === undefined){
+                type = $(e.target).parent().data('type');
+            }
             var name = webgnome.model.get('name') ? webgnome.model.get('name') + ' ICS 209' : 'ICS 209';
             var filename = name + '.' + type;
             var content = '';
