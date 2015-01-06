aa4fbc087ab32749bf4103d3f8ab7a0489bb4954
Alexander Miranda <alex.miranda@noaa.gov>
2015-01-06 11:57:20 -0800

    Resolved issue #107 by commenting out the validation code for the emulsion override model

--- js/model/substance.js
+++ js/model/substance.js
@@ -26,21 +26,21 @@ define([
         },
 
         validate: function(attrs, options){
-            if (_.isUndefined(attrs.bullwinkle_fraction)){
-                return 'Stable emulsion fraction must be defined!';
-            }
+            // if (_.isUndefined(attrs.bullwinkle_fraction)){
+            //     return 'Stable emulsion fraction must be defined!';
+            // }
 
-            if (_.isUndefined(attrs.emulsion_water_fraction_max)){
-                return 'Emulsion constant must be defined!';
-            }
+            // if (_.isUndefined(attrs.emulsion_water_fraction_max)){
+            //     return 'Emulsion constant must be defined!';
+            // }
             
-            if (!_.isNumber(attrs.bullwinkle_fraction) || (attrs.bullwinkle_fraction < 0 || attrs.bullwinkle_fraction > 1)){
-                return 'Stable emulsion fraction must be a number between zero and one!';
-            }
+            // if (!_.isNumber(attrs.bullwinkle_fraction) || (attrs.bullwinkle_fraction < 0 || attrs.bullwinkle_fraction > 1)){
+            //     return 'Stable emulsion fraction must be a number between zero and one!';
+            // }
 
-            if (!_.isNumber(attrs.emulsion_water_fraction_max) || (attrs.emulsion_water_fraction_max < 0 || attrs.emulsion_water_fraction_max > 1)){
-                return 'Emulsion constant must be a number between zero and one!';
-            }
+            // if (!_.isNumber(attrs.emulsion_water_fraction_max) || (attrs.emulsion_water_fraction_max < 0 || attrs.emulsion_water_fraction_max > 1)){
+            //     return 'Emulsion constant must be a number between zero and one!';
+            // }
         },
 
         parseCategories: function(){
