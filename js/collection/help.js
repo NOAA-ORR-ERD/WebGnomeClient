8efee891bf81a9b998258e67ef03933588a108db
Alexander Miranda <alex.miranda@noaa.gov>
2015-01-09 09:51:52 -0800

    Commiting progress on issue #85

---
+++ js/collection/help.js
@@ -0,0 +1,11 @@
+define([
+    'underscore',
+    'backbone',
+    'model/help/help'
+], function(_, Backbone, HelpModel){
+    var gnomeHelpCollection = Backbone.Collection.extend({
+        model: HelpModel
+    });
+
+    return gnomeHelpCollection;
+});
--- js/views/default/faq.js
+++ js/views/default/faq.js
@@ -4,8 +4,9 @@ define([
     'backbone',
     'text!templates/default/faq.html',
     'text!templates/faq/specific.html',
-    'text!templates/faq/default.html'
-], function($, _, Backbone, FAQTemplate, SpecificTemplate, DefaultTemplate){
+    'text!templates/faq/default.html',
+    'model/help/help'
+], function($, _, Backbone, FAQTemplate, SpecificTemplate, DefaultTemplate, HelpModel){
 	var faqView = Backbone.View.extend({
         className: 'page faq',
 
@@ -14,7 +15,8 @@ define([
             'click .build': 'build',
             'click .load': 'load',
             'focus #helpquery': 'renderContent',
-            'click .back': 'restoreDefault'
+            'click .back': 'restoreDefault',
+            'click h4': 'specificHelp'
         },
 
         initialize: function(){
@@ -25,6 +27,29 @@ define([
             var subtemplate = _.template(DefaultTemplate, {});
             var compiled = _.template(FAQTemplate, {content: subtemplate});
             $('body').append(this.$el.append(compiled));
+            var helpModel = new HelpModel();
+            helpModel.fetch({
+                success: _.bind(function(model){
+                    this.questions = model;
+                    this.parseHelp(model);
+                }, this)
+            });
+        },
+
+        parseHelp: function(model){
+            for (var i = 0; i < 20; i++){
+                console.log(this.questions);
+                if (_.isObject(model.attributes[i])){
+                    var helpHTML = $('<div />').append(model.attributes[i].html);
+                    var path = model.attributes[i].path;
+                    var helpTitle = $($.parseHTML(helpHTML[0].innerHTML)).find('h1')[i].innerHTML;
+                    this.$('.helpcontent').append('<div class="col-md-6"><h4 data-path="' + path + '">' + helpTitle + '</h4></div>');
+                }
+            }
+        },
+
+        specificHelp: function(e){
+            console.log(e.target.dataset);
         },
 
         renderContent: function(){
