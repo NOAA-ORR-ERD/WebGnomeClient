9c4490cc640ec74376e820fbfc47a79429fe41c6
Alexander Miranda <alex.miranda@noaa.gov>
2015-02-25 14:16:58 -0800

    Resolved issue #121

--- js/views/model/fate.js
+++ js/views/model/fate.js
@@ -913,6 +913,10 @@ define([
 
                     nominal_value = nominal[this.dataset[set].name];
                     nominal_value = converter.Convert(nominal_value, 'kg', api, 'API degree', units);
+                }  else if (this.dataset[set].name === 'avg_viscosity') {
+                    low_value = low[this.dataset[set].name] * 1000000;
+                    nominal_value = nominal[this.dataset[set].name] * 1000000;
+                    high_value = high[this.dataset[set].name] * 1000000;
                 } else {
                     low_value = low[this.dataset[set].name];
                     nominal_value = nominal[this.dataset[set].name];
@@ -920,7 +924,6 @@ define([
                 }
 
                 
-
                 this.dataset[set].high.push([date.unix() * 1000, high_value]);
                 this.dataset[set].low.push([date.unix() * 1000, low_value]);
                 this.dataset[set].data.push([date.unix() * 1000, nominal_value, 0, low_value, high_value]);
