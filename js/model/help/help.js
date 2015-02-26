70b80d8bcd7ae12c1fafbe594b58bab46be224fc
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-02-26 09:05:32 -0800

    added parse to help that will shift tabs and tab-pans into the 'help-' namespace/id prefix to prevent other view tabs with the same name from triggering instead of the help tab-pane. this was occuring on the fate view

--- js/model/help/help.js
+++ js/model/help/help.js
@@ -11,6 +11,18 @@ define([
             helpful: false
         },
 
+        parse: function(response){
+            var docs = $('<div>' + response.html + '</div>').find('.document');
+            var html = '';
+            _.each(docs, function(doc){
+                $(doc).attr('id', 'help-' + $(doc).attr('id'));
+                html += $('<div>').append(doc).html();
+            });
+            response.html = html;
+            console.log(response.html);
+            return response;
+        },
+
         makeExcerpt: function(){
             var topicBody = $('<div>' + this.get('html') + '</div>');
             topicBody.find('h1').remove();
