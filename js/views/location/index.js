4cafa6cb11cebf97d7b70f986a0d8d7b5a53cf69
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-03-13 09:34:46 -0700

    improved pin click intereaction for loaction select map fixes #179

--- js/views/location/index.js
+++ js/views/location/index.js
@@ -64,35 +64,46 @@ define([
                     return feature;
                 });
                 if(feature){
-                    this.popup.setPosition(feature.getGeometry().getCoordinates());
-                    this.$('.popup').popover('destroy');
-                    this.$('.popup').popover({
-                        placement: 'top',
-                        html: true,
-                        title: feature.get('title'),
-                        content: '<button class="btn btn-primary load" data-slug="' + feature.get('slug') + '" data-name="' + feature.get('title') + '">Load</button>'
-                    });
-                    
-                    this.$('.popup').one('shown.bs.popover', _.bind(function(){
-                        this.$('.load').on('click', _.bind(function(){
-                            var slug = this.$('.load').data('slug');
-                            var name = this.$('.load').data('name');
-                            webgnome.model.resetLocation(_.bind(function(){
-                                this.load({slug: slug, name: name});
-                                this.$('.popup').popover('destroy');
-                            }, this));
+                    if(this.$('.popover').length === 0){
+                        this.clickPin(feature);
+                    } else {
+                        this.$('.popup').one('hidden.bs.popover', _.bind(function(){
+                            setTimeout(_.bind(function(){
+                                this.clickPin(feature);
+                            }, this), 1);
                         }, this));
-                    }, this));
-
-                    this.$('.popup').one('hide.bs.popover', _.bind(function(){
-                        this.$('.load').off('click');
-                    }, this));
-
-                    this.$('.popup').popover('show');
+                        this.$('.popup').popover('destroy');
+                    }
                 } else {
                     this.$('.popup').popover('destroy');
                 }
             }, this);
+        },
+
+        clickPin: function(feature){
+            this.popup.setPosition(feature.getGeometry().getCoordinates());
+            this.$('.popup').popover({
+                placement: 'top',
+                html: true,
+                title: feature.get('title'),
+                content: '<button class="btn btn-primary load" data-slug="' + feature.get('slug') + '" data-name="' + feature.get('title') + '">Load</button>'
+            });
+            this.$('.popup').popover('show');
+
+            this.$('.popup').one('shown.bs.popover', _.bind(function(){
+                this.$('.load').on('click', _.bind(function(){
+                    var slug = this.$('.load').data('slug');
+                    var name = this.$('.load').data('name');
+                    webgnome.model.resetLocation(_.bind(function(){
+                        this.load({slug: slug, name: name});
+                        this.$('.popup').popover('destroy');
+                    }, this));
+                }, this));
+            }, this));
+
+            this.$('.popup').one('hide.bs.popover', _.bind(function(){
+                this.$('.load').off('click');
+            }, this));
         },
 
         load: function(options){
