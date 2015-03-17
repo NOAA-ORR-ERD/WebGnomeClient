053963d47422b4832a534103788054a00932b474
Alexander Miranda <alex.miranda@noaa.gov>
2015-03-17 13:09:47 -0700

    User can no longer delete wind time series entry while actively editing another wind entry

--- js/views/form/wind.js
+++ js/views/form/wind.js
@@ -418,11 +418,13 @@ define([
         },
 
         removeTimeseriesEntry: function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            var index = e.target.parentElement.parentElement.dataset.tsindex;
-            this.model.get('timeseries').splice(index, 1);
-            this.renderTimeseries();
+            if (this.$('.input-speed').length === 0){
+                e.preventDefault();
+                e.stopPropagation();
+                var index = e.target.parentElement.parentElement.dataset.tsindex;
+                this.model.get('timeseries').splice(index, 1);
+                this.renderTimeseries();
+            }
         },
 
         renderTimeseries: function(uncertainty){
