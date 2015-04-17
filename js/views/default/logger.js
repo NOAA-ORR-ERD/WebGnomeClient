790d5031621f42fe0240077bce254ad0bc63bfa8
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-04-17 10:45:33 -0700

    added logging method and some basic logging

--- js/views/default/logger.js
+++ js/views/default/logger.js
@@ -27,15 +27,54 @@ define([
             var compiled = _.template(LoggerTemplate);
             this.$el.append(LoggerTemplate);
             $('body').append(this.$el);
+
+            if(localStorage.getItem('logger') != 'null'){
+                this.toggle();
+            }
         },
 
-        toggle: function(){
+        toggle: function(e){
             $('body').toggleClass('logger-open');
             this.$el.toggleClass('open');
+
+            if(e){
+                if(localStorage.getItem('logger') != 'null'){
+                    localStorage.setItem('logger', null);
+                } else {
+                    localStorage.setItem('logger', true);
+                }
+            }
         },
 
         startSocket: function(){
+            this.log('Connecting...');
             this.socket = new WebSocket(this.socketUrl);
+            this.socket.onerror = _.bind(this.socketError, this);
+            this.socket.onconnect = _.bind(this.socketConnect, this);
+        },
+
+        socketError: function(error){
+            this.log({type: 'error', message: 'Failed to connect!'});
+        },
+
+        socketConnect: function(){
+            this.log('Connected!');
+        },
+
+        /**
+         * Print a log message to the window
+         * @param  {Object or String}
+         */
+        log: function(message){
+            if(_.isString(message)){
+                this.$('.window .logs').append('<li>' + message + '</li>');
+                return;
+            }
+
+            if(_.isObject(message)){
+                this.$('.window .logs').append('<li class="' + message.type + '">' + message.message + '</li>');
+                return;
+            }
         }
     });
 
