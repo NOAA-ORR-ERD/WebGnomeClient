75b465b1b027bd968dd4aa5e12fe7ee07fc304bb
Alexander Miranda <alex.miranda@noaa.gov>
2015-04-21 14:16:52 -0700

    Fixes #205

--- js/views/model/fate.js
+++ js/views/model/fate.js
@@ -850,7 +850,7 @@ define([
             var converter = new nucos.OilQuantityConverter();
 
             for(var set in this.dataset){
-                if(['natural_dispersion', 'evaporated', 'floating', 'amount_released', 'skimmed', 'burned', 'beached'].indexOf(this.dataset[set].name) !== -1){
+                if(['natural_dispersion', 'dispersed', 'evaporated', 'floating', 'amount_released', 'skimmed', 'burned', 'beached'].indexOf(this.dataset[set].name) !== -1){
                     min = _.min(step.get('WeatheringOutput'), function(run){
                         return run[this.dataset[set].name];
                     }, this);
