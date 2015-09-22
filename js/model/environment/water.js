bd511c87dec574ac3de39ed38e9f0ceb386c23fa
Alexander Miranda <alex.miranda@noaa.gov>
2015-09-22 13:31:11 -0700

    Added upper bound validation for the wave height attribute on the water model

--- js/model/environment/water.js
+++ js/model/environment/water.js
@@ -49,11 +49,24 @@ define([
                 return 'Water temperature must be a reasonable degree.';
             }
 
-            if (attrs.wave_height < 0 || attrs.wave_height === ''){
+            if (attrs.wave_height === ''){
+                return 'A value for wave height must be inputted!';
+            }
+
+            if (attrs.wave_height < 0){
                 return 'Wave height must be a number greater than or equal to zero!';
             }
 
-            if (attrs.fetch < 0 || attrs.fetch === ''){
+            if (nucos.convert('Length', attrs.units.wave_height, 'm', attrs.wave_height) > 15.5){
+                var upperBound = Math.round(nucos.convert('Length', 'm', attrs.units.wave_height, 15.5));
+                return 'Wave height cannot be greater than ' + upperBound + ' ' + attrs.units.wave_height + '!';
+            }
+
+            if (attrs.fetch === ''){
+                return 'A value for fetch must be inputted!';
+            }
+
+            if (attrs.fetch < 0){
                 return 'Fetch must be a number greater than or equal to zero!';
             }
 
