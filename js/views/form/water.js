ba18d13e9ea27d6a4f79d94abe0e8ec3584c83c2
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-09-23 17:46:25 -0700

    fixed temp validation, previously could submit form w/o temp input. made wave height validation lazier #366 #362

--- js/views/form/water.js
+++ js/views/form/water.js
@@ -130,7 +130,6 @@ define([
             } else if (value === 'windcalc') {
                 this.$('.fetch, .specified').addClass('hide');
             }
-            this.update();
         }
 
     });
