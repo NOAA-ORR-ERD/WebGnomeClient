b5740a04b2ba34724a32ca8b873ae896905639e1
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-11-25 13:42:48 -0800

    really adding base map

---
+++ js/model/map/base.js
@@ -0,0 +1,69 @@
+define([
+    'model/base',
+    'ol'
+], function(BaseModel, ol){
+    var baseMap = BaseModel.extend({
+        urlRoot: '/map/',
+        requesting: false,
+        requested: false,
+        geo_json: '',
+        
+        getExtent: function(){
+            var extent;
+            if (!_.isUndefined(this.get('spillable_area'))){
+                if (this.get('spillable_area').length === 1){
+                    extent = ol.extent.boundingExtent(this.get('spillable_area')[0]);
+                } else {
+                    var areas = this.get('spillable_area');
+                    extent = ol.extent.boundingExtent([]);
+                    for (var i = 0; i < areas.length; i++){
+                        var tempExtent = ol.extent.boundingExtent(areas[i]);
+                        extent = ol.extent.extend(extent, tempExtent);
+                    }
+                }
+            } else {
+                extent = ol.extent.boundingExtent(this.get('map_bounds'));
+            }
+            return extent;
+        },
+
+        getSpillableArea: function(){
+            var boundingPolygon;
+            if (!_.isUndefined(this.get('spillable_area'))){
+                if (this.get('spillable_area').length === 1){
+                    boundingPolygon = new ol.geom.Polygon(this.get('spillable_area'));
+                } else {
+                    var area = [];
+                    for(var a = 0; a < this.get('spillable_area').length; a++){
+                        area.push(new ol.geom.Polygon([this.get('spillable_area')[a]]));
+                    }
+                    boundingPolygon = area;
+                }
+            } else {
+                boundingPolygon = new ol.geom.Polygon(this.get('map_bounds'));
+            }
+            return boundingPolygon;
+        },
+
+        resetRequest: function(){
+            this.requested = false;
+        },
+
+        getGeoJSON: function(callback){
+            var url = webgnome.config.api + this.urlRoot + this.get('id') + '/geojson';
+            if(!this.requesting && !this.requested){
+                this.requesting = true;
+                $.get(url, null, _.bind(function(geo_json){
+                    this.requesting = false;
+                    this.requested = true;
+                    this.geo_json = geo_json;
+                    callback(geo_json);
+                }, this));
+            } else {
+                callback(this.geo_json);
+            }
+        }
+    });
+
+    return baseMap;
+});
