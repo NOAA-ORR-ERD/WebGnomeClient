495a474f8b6c03f5257ca2e3b59b5511c3d9f8c6
Nathan Wilkins <nathan.wilkins@noaa.gov>
2015-12-01 12:56:05 -0800

    If the model has any messages, don't auto play. This should allow for the visualization of currents and shorelines w/o trying to run a model.

--- js/views/model/trajectory.js
+++ js/views/model/trajectory.js
@@ -475,7 +475,9 @@ define([
 
         load: function(){
             this.updateProgress();
-            this.play();
+            if(webgnome.model.get('messages').length < 1){
+                this.play();
+            }
             webgnome.cache.on('step:recieved', this.renderStep, this);
             webgnome.cache.on('step:failed', this.pause, this);
         },
