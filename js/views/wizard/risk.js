b1aa6bf450c83ef6e5e10add96c2d949ce7e11e1
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-16 10:51:33 -0800

    Param map form is only added to risk wizard steps if the gnome map is undefined

--- js/views/wizard/risk.js
+++ js/views/wizard/risk.js
@@ -18,13 +18,15 @@ define([
 
         setup: function(riskModel){
             this.steps = [
-                new ParamMapForm(),
                 new TuningForm({
                     name: 'step2',
                     title: 'Environmental Risk Assessment <span class="sub-title">Tuning</span>',
                     buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="back">Back</button><button type="button" class="save">Save</button>',
                 }, riskModel)
             ];
+            if (webgnome.model.get('map').get('obj_type') === 'gnome.map.GnomeMap') {
+                this.steps.unshift(new ParamMapForm());
+            }
             this.start();
         },
 
