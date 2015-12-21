2d6472a9fda43de4e46e7142dccdd7a82e20420b
Alexander Miranda <alex.miranda@noaa.gov>
2015-12-21 09:41:23 -0800

    Changed the sub title for the input form for the risk wizard

--- js/views/wizard/risk.js
+++ js/views/wizard/risk.js
@@ -21,7 +21,7 @@ define([
             this.steps = [
                 new InputForm({
                     name: 'step1',
-                    title: 'Environmental Risk Assessment <span class="sub-title">Tuning</span>',
+                    title: 'Environmental Risk Assessment <span class="sub-title">Water Depth</span>',
                     buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="save">Save</button>'
                 }, riskModel),
                 new TuningForm({
