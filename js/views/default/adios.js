415cd15f55b779c322af99d5816d7c1ade4c7a82
Nathan Wilkins <nathan.wilkins@noaa.gov>
2016-01-03 14:08:49 -0800

    prototype adios view

--- js/views/default/adios.js
+++ js/views/default/adios.js
@@ -2,16 +2,16 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/default/adios.html'
-], function($, _, Backbone, AdiosTemplate){
+    'text!templates/default/adios.html',
+    'views/form/oil/library',
+    'model/element'
+], function($, _, Backbone, AdiosTemplate, OilLibraryView, ElementType){
     'use strict';
     var adiosView = Backbone.View.extend({
-        className: 'container page adios',
+        className: 'page adios',
 
         events: {
-            'click .resume': 'resume',
-            'click .build': 'build',
-            'click .load': 'load',
+            'click .substance': 'clickSubstance'
         },
 
         initialize: function(){
@@ -19,25 +19,36 @@ define([
         },
 
         render: function(){
+            var substance = false;
+            var spills = webgnome.model.get('spills').length === 0;
+            var wind = webgnome.model.get('environment').findWhere({obj_type: 'gnome.environment.wind.Wind'});
+            var water = webgnome.model.get('environment').findWhere({obj_type: 'gnome.environment.environment.Water'});
             var compiled = _.template(AdiosTemplate, {
-                hasModel: webgnome.hasModel()
+                substance: substance,
+                spills: spills,
+                wind: wind, 
+                water: water
             });
             $('body').append(this.$el.append(compiled));
         },
 
-        resume: function(e){
-            e.preventDefault();
-            webgnome.router.navigate('adios/model', true);
+        clickSubstance: function(){
+            var spills = webgnome.model.get('spills');
+            var element_type;
+            if(spills.length > 0){
+                element_type = webgnome.model.get('spills').at(0).get('element_type');
+            } else {
+                element_type = new ElementType();
+            }
+            var oilLib = new OilLibraryView({}, element_type);
+            oilLib.on('save wizardclose', _.bind(function(){
+                console.log(element_type);
+                webgnome.model.save();
+                oilLib.on('hidden', oilLib.close);
+            }, this));
+            oilLib.render();
         },
 
-        build: function(e){
-            e.preventDefault();
-            webgnome.router.navigate('adios/setup', true);
-        },
-
-        load: function(e){
-            e.preventDefault();
-        }
     });
 
     return adiosView;
