3c3083489ff947894ffaf1e7bee21ac472e637bf
Alexander Miranda <alex.miranda@noaa.gov>
2016-01-11 12:28:41 -0800

    Chemical dispersion duration now defaults to one webgnome model's time step in length, issue #444

--- js/model/weatherers/dispersion.js
+++ js/model/weatherers/dispersion.js
@@ -1,9 +1,10 @@
 define([
     'underscore',
     'backbone',
+    'moment',
     'model/weatherers/base',
     'model/environment/waves'
-], function(_, Backbone, BaseModel, WavesModel){
+], function(_, Backbone, moment, BaseModel, WavesModel){
     'use strict';
     var dispersionWeatherer = BaseModel.extend({
         defaults: {
@@ -17,10 +18,30 @@ define([
             waves: WavesModel
         },
 
+        initialize: function() {
+            BaseModel.prototype.initialize.call(this);
+            if (_.has(window, 'webgnome') && _.has(webgnome, 'model') && !_.isNull(webgnome.model)) {
+                this.resetEndTime();
+            }
+            this.on('change:active_start', this.resetEndTime, this);
+        },
+
         validate: function(attrs, options){
             if (attrs.fraction_sprayed <= 0){
                 return 'Percent of oil sprayed must be greater than zero!';
             }
+        },
+
+        resetEndTime: function() {
+            var timeStepLength;
+            if (_.has(window, 'webgnome') && _.has(webgnome, 'model') && !_.isNull(webgnome.model)){
+                timeStepLength = parseFloat(webgnome.model.get('time_step'));
+            } else {
+                timeStepLength = 900;
+            }
+            var start_time = moment(this.get('active_start')).unix();
+            var new_end_time = moment((start_time + timeStepLength) * 1000);
+            this.set('active_stop', new_end_time.format());
         },
 
         toTree: function(){
--- js/views/form/response/disperse.js
+++ js/views/form/response/disperse.js
@@ -26,7 +26,6 @@ define([
             this.body = _.template(FormTemplate,{
                 name: this.model.get('name'),
                 time: this.model.get('active_start') !== '-inf' ? moment(this.model.get('active_start')).format('YYYY/M/D H:mm') : moment(webgnome.model.get('start_time')).format('YYYY/M/D H:mm'),
-                duration: this.parseDuration(this.model.get('active_start'), this.model.get('active_stop')),
                 percentSprayed: !_.isUndefined(fractSprayed) ? fractSprayed * 100 : 0
             });
             ResponseFormModal.prototype.render.call(this, options);
@@ -35,14 +34,11 @@ define([
         update: function(){
             ResponseFormModal.prototype.update.call(this);
 
-            this.model.set('active_start', this.startTime.format('YYYY-MM-DDTHH:mm:ss'));
+            //this.model.set('active_start', this.startTime.format('YYYY-MM-DDTHH:mm:ss'));
 
-            var duration = parseFloat(this.$('#duration').val());
-            var endTime = this.startTime.add(duration, 'h').format('YYYY-MM-DDTHH:mm:ss');
             var sprayedOilPercent = this.$('#oilsprayed').val();
             var dispersedOilPercent = this.$('#oildispersed').val();
 
-            this.model.set('active_stop', endTime);
             this.model.set('fraction_sprayed', sprayedOilPercent / 100);
         }
     });
