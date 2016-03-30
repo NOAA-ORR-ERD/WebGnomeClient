e2ed9f010b832b3bfda1cad97ce6abb5765c118d
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2016-03-30 16:52:19 -0700

    changed the condition to match the error - manual beaching can't start until after model start time

--- js/model/weatherers/manual_beaching.js
+++ js/model/weatherers/manual_beaching.js
@@ -35,7 +35,7 @@ define([
 					}
 					var beachedStartTime = moment(el[0]).unix();
 
-					if (beachedStartTime < modelStartTime){
+					if (beachedStartTime <= modelStartTime){
 						msg = 'Beaching events must happen after the gnome model start time!';
 					}
 
