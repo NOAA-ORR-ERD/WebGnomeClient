f5c1ab35280b2e2812acf9ff732b505f5dca4fec
Alexander Miranda <alex.miranda@noaa.gov>
2016-05-04 12:55:14 -0700

    Changed button label Finalize Model to Run Model in the location wizard, issue #574

--- js/views/wizard/location.js
+++ js/views/wizard/location.js
@@ -91,7 +91,7 @@ define([
                         name: el.name,
                         title: title.join(' '),
                         body: el.body,
-                        buttons: "<button type='button' class='cancel' data-dismiss='modal'>Cancel</button><button type='button' class='back'>Back</button><button type='button' class='next'>Next</button>"
+                        buttons: "<button type='button' class='cancel'>Cancel</button><button type='button' class='back'>Back</button><button type='button' class='next'>Next</button>"
                     }, wind);
                     windform.on('save', _.bind(function(){
                         webgnome.model.get('environment').add(wind, {merge: true});
@@ -116,7 +116,7 @@ define([
                         name: el.name,
                         title: title.join(' '),
                         body: "<div>Filler Text</div>",
-                        buttons: "<button type='button' class='cancel' data-dismiss='modal'>Cancel</button><button type='button' class='back'>Back</button><button type='button' class='finish' data-dismiss='modal'>Finalize Model</button>"
+                        buttons: "<button type='button' class='cancel' data-dismiss='modal'>Cancel</button><button type='button' class='back'>Back</button><button type='button' class='finish' data-dismiss='modal'>Run Model</button>"
                     });
                     finishForm.on('finish', function(){
                         webgnome.model.save().always(function(){
