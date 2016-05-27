64d37e503eb25910581265d719385bb938c54322
Nathan Wilkins <nathan.wilkins@noaa.gov>
2016-05-27 09:44:52 -0700

    added input disabled-enable contextualization to skim form

--- js/views/form/response/roc_skim.js
+++ js/views/form/response/roc_skim.js
@@ -10,16 +10,48 @@ define([
         title: 'ROC Skim Response',
         size: 'lg',
 
+        initialize: function(options){
+            BaseResponseForm.prototype.initialize.call(this, options);
+            this.listenTo(this.model, 'change:offload_to', this.toggleBargeTime);
+            this.listenTo(this.model, 'change:recovery', this.toggleRecovery);
+        },
+
         render: function(){
             var compiled = _.template(ROCSkimTemplate, this.model.attributes);
             this.body = compiled;
             BaseResponseForm.prototype.render.call(this);
             this.timeseries = new TimeseriesView({model: this.model});
             this.$('.timeseries').append(this.timeseries.$el);
+
+            this.$('#barge_arrival').datetimepicker({
+                format: webgnome.config.date_format.datetimepicker,
+                allowTimes: webgnome.config.date_format.half_hour_times,
+                step: webgnome.config.date_format.time_step
+            });
+
+            this.toggleBargeTime();
+            this.toggleRecovery();
+        },
+
+        toggleBargeTime: function(){
+            if(this.model.get('offload_to') === 'shore'){
+                this.$('#barge_arrival').attr('disabled', true);
+            } else {
+                this.$('#barge_arrival').attr('disabled', false);
+            }
+        },
+
+        toggleRecovery: function(){
+            if(this.model.get('recovery') === 'custom'){
+                this.$('#recovery_ef').attr('disabled', false);
+            } else {
+                this.$('#recovery_ef').attr('disabled', true);
+            }
         },
 
         close: function(){
             this.timeseries.close();
+            $('.xdsoft_datetimepicker:last').remove();
             BaseResponseForm.prototype.close.call(this);
         }
     });
