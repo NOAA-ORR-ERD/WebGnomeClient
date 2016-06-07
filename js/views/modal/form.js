f159053c64c526c8331eee7901a15ae8fe832219
Nathan Wilkins <nathan.wilkins@noaa.gov>
2016-06-07 14:46:59 -0700

    re-organized form modal module.
    moved form related things into it's own class and composed the modal form out of the base modal and base form modules.

--- js/views/modal/form.js
+++ js/views/modal/form.js
@@ -4,12 +4,14 @@ define([
     'backbone',
     'moment',
     'views/modal/base',
+    'views/form/base',
     'text!templates/default/alert-danger.html',
     'views/default/help',
-    'views/attributes/attributes',
-], function($, _, Backbone, moment, BaseModal, AlertDangerTemplate, HelpView, AttributesView){
+    'views/attributes/attributes'
+], function($, _, Backbone, moment, BaseModal, BaseForm, AlertDangerTemplate, HelpView, AttributesView){
     'use strict';
-    var formModal = BaseModal.extend({
+    var PseudoMultipleInheritance = BaseForm.extend(BaseModal.prototype);
+    var formModal = PseudoMultipleInheritance.extend({
         className: 'modal form-modal',
         buttons: '<button type="button" class="cancel" data-dismiss="modal">Cancel</button><button type="button" class="save">Save</button>',
         form: [],
@@ -39,79 +41,6 @@ define([
             }
         },
 
-        update: function(e){
-            var name = $(e.target).attr('name');
-            var value = $(e.target).val();
-            if(!name){ return; }
-            // if the user is inputting a negative numerical value
-            // reset it back to the non-neg version.
-            if(value < 0 || value === '-'){
-                nonneg = value.replace('-', '');
-                $(e.target).val(parseFloat(nonneg));
-                value = nonneg; 
-            }
-
-            if($(e.target).attr('type') === 'number'){
-                value = parseFloat(value);
-            }
-
-            name = name.split(':');
-            if(name.length === 1){
-                this.model.set(name[0], value);
-            } else {
-                this.model.get(name[0]).set(name[1], value);
-            }
-        },
-
-        sync: function(){
-            // supports second level nested models only
-            // hopefully shouldn't need more than that
-            if(_.isUndefined(this.model)){ return; }
-
-            var names = _.keys(this.model.attributes);
-            for(var name in names){
-                var field = this.$('[name="' + names[name] + '"]');
-                if(field.length > 0){
-                    this.setInputVal(field, this.model.get(names[name]));
-                } else {
-                    var nested_model = this.model.get(names[name]);
-                    if(nested_model){
-                        var nested_names = _.keys(nested_model.attributes);
-                        for(var nested_name in nested_names){
-                            var nested_field = this.$('[name="' + names[name] + ':' + nested_names[nested_name] + '"]');
-                            if(nested_field.length > 0){
-                                this.setInputVal(nested_field, this.model.get(names[name]).get(nested_names[nested_name]));
-                            }
-                        }
-                    }
-                }
-            }
-        },
-
-        setInputVal: function(el, val){
-            if(el.is('input[type="text"]') && _.isString(val) && val.match(/^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/) !== null){
-                el.val(moment(val).format(webgnome.config.date_format.moment));
-            } else if(el.is('textarea') || el.is('input[type="number"]') || el.is('input[type="text"]')){
-                el.val(val);
-            } else if (el.is('input[type="radio"]')){
-                for(var r = 0; r < el.length; r++){
-                    if($(el[r]).attr('value') === val){
-                        $(el[r]).attr('checked', true);
-                    }
-                }
-            } else if(el.is('select')){
-                el.val(val);
-            }
-        },
-
-        selectContents: function(e){
-            var type = this.$(e.target).attr('type');
-            if (type === 'number' || type === 'text') {
-                e.preventDefault();
-                this.$(e.target).select();
-            }
-        },
-
         render: function(){
             BaseModal.prototype.render.call(this);
             if(this.model){
@@ -119,14 +48,6 @@ define([
                 this.renderAttributes();
                 this.model.on('change', this.renderAttributes, this);
             }
-        },
-
-        renderAttributes: function(){
-            if(this.attributes){
-                this.attributes.remove();
-            }
-            this.attributes = new AttributesView({name: this.model.get('obj_type'), model: this.model});
-            this.$('.modal-body').append(this.attributes.$el);
         },
 
         stickyFooter: function(){
@@ -233,6 +154,14 @@ define([
             this.trigger('back');
         },
 
+        renderAttributes: function(){
+            if(this.attributes){
+                this.attributes.remove();
+            }
+            this.attributes = new AttributesView({name: this.model.get('obj_type'), model: this.model});
+            this.$('.modal-body').append(this.attributes.$el);
+        },
+
         error: function(strong, message) {
             this.$('.modal-body .alert.validation').remove();
             this.$('.modal-body').prepend(_.template(AlertDangerTemplate, {strong: strong, message: message}));
@@ -240,29 +169,6 @@ define([
 
         clearError: function() {
             this.$('.modal-body .alert.validation').remove();
-        },
-
-        isValid: function() {
-            if (_.isFunction(this.validate)){
-                var valid = this.validate();
-                if (_.isUndefined(valid)) {
-                    this.validationError = null;
-                    return true;
-                }
-                this.validationError = valid;
-                return false;
-            } else {
-                return true;
-            }
-        },
-
-        validate: function() {
-            if (!_.isUndefined(this.model)) {
-                if (this.model.isValid()) {
-                    return;
-                }
-                return this.model.validationError;
-            }
         },
 
         wizardclose: function(){
