8785397ae61bf86f6f1e3b5929f900316aefe314
Alexander Miranda <alex.miranda@noaa.gov>
2016-06-14 14:05:03 -0700

    Updated the Help I'm stuck link to produce a modal advising the user to refresh the page and email support if the problem persists, issue #706

--- js/views/default/footer.js
+++ js/views/default/footer.js
@@ -5,16 +5,19 @@ define([
     'text!templates/default/footer/footer.html',
     'views/modal/base',
     'text!templates/default/footer/disclaimer.html',
-    'text!templates/default/footer/privacy.html'
-], function($, _, Backbone, FooterTemplate, BaseModal, DisclaimerTemplate, PrivacyTemplate){
+    'text!templates/default/footer/privacy.html',
+    'text!templates/default/footer/troubleshooting.html'
+], function($, _, Backbone, FooterTemplate, BaseModal, DisclaimerTemplate, PrivacyTemplate, TroubleShootingTemplate){
     'use strict';
     var footerView = Backbone.View.extend({
         className: 'footer',
         rendered: false,
+        modalButtons: '<a href="" class="btn btn-primary" data-dismiss="modal">Ok</a>',
 
         events: {
             'click .disclaimer': 'disclaimer',
-            'click .privacy': 'privacy'
+            'click .privacy': 'privacy',
+            'click .help': 'helpPopup'
         },
 
         initialize: function(){
@@ -35,19 +38,29 @@ define([
             var modal = new BaseModal({
                 title: 'Disclaimer',
                 body: DisclaimerTemplate,
-                buttons: '<a href="" class="btn btn-primary" data-dismiss="modal">Ok</a>'
+                buttons: this.modalButtons
             });
             modal.render();
-        }, 
+        },
 
         privacy: function(e){
             e.preventDefault();
             var modal = new BaseModal({
                 title: 'Privacy Policy',
                 body: PrivacyTemplate,
-                buttons: '<a href="" class="btn btn-primary" data-dismiss="modal">Ok</a>'
+                buttons: this.modalButtons
             });
-            modal.render(); 
+            modal.render();
+        },
+
+        helpPopup: function(e) {
+            e.preventDefault();
+            var modal = new BaseModal({
+                title: 'Trouble Shooting',
+                body: TroubleShootingTemplate,
+                buttons: this.modalButtons
+            });
+            modal.render();
         }
     });
 
