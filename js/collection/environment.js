948fd8dbb68edf2ac9126721b844c50e12819431
Alexander Miranda <alex.miranda@noaa.gov>
2016-07-19 09:54:43 -0700

    Refactored the windCompliance method to output the same data schema as the mover's compliance method, issue #714

--- js/collection/environment.js
+++ js/collection/environment.js
@@ -7,17 +7,20 @@ define([
     var environment = Backbone.Collection.extend({
 
         windCompliance: function() {
+            var modelStart = moment(this.get('start_time')).unix();
+            var modelEnd = moment(this.get('start_time')).add(this.get('duration'), 'm').unix();
             var windData = this.findWhere({'obj_type': 'gnome.environment.wind.Wind'});
             var windArr = [];
 
-            if (!_.isUndefined(windData)) {
-                var ts = windData.get('timeseries');
+            if (!_.isUndefined(windData) && !_.isUndefined(windData.get('timeseries'))) {
+                var ts = !_.isUndefined(windData.get('timeseries')) ? windData.get('timeseries') : [];
                 var windStart = moment(ts[0][0]).unix();
                 var windEnd = moment(ts[ts.length - 1][0]).unix();
+                var timeDiff;
 
                 if (windStart > modelStart || windEnd < modelEnd) {
-                    var startDiff = !_.isNaN(start - modelStart) ? start - modelStart : 0;
-                    var endDiff = !_.isNaN(modelEnd - end) ? modelEnd - end : 0;
+                    var startDiff = !_.isNaN(windStart - modelStart) ? windStart - modelStart : 0;
+                    var endDiff = !_.isNaN(modelEnd - windEnd) ? modelEnd - windEnd : 0;
 
                     if (startDiff > endDiff) {
                         timeDiff = parseInt(startDiff / 60, 10);
@@ -25,7 +28,7 @@ define([
                         timeDiff = parseInt(endDiff / 60, 10);
                     }
 
-                    windArr.push({name: windData.get('name'), timeDiff: timeDiff});
+                    windArr.push({model: windData, timeDiff: timeDiff});
                 }
             }
 
