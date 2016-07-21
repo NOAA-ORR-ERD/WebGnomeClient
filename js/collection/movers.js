d10a83071a23dd389a37dfa34d2449e4ac8964db
Alexander Miranda <alex.miranda@noaa.gov>
2016-07-21 13:35:48 -0700

    Changes to mvovers collection for issue #714

--- js/collection/movers.js
+++ js/collection/movers.js
@@ -1,13 +1,57 @@
 define([
     'underscore',
     'backbone',
-    'moment'
-], function(_, Backbone, moment){
+    'moment',
+    'sweetalert'
+], function(_, Backbone, moment, swal){
     'use strict';
     var movers = Backbone.Collection.extend({
 
         convertToMinutes: function(secs) {
             return parseInt(secs / 60, 10);
+        },
+
+        findValidTimeInterval: function() {
+            var start, end, prevMover;
+
+            this.each(_.bind(function(el, i, col){
+                if (!el.get('extrapolate') && el.get('on')) {
+                    if ((!_.isUndefined(start) && !_.isUndefined(end)) || (el.get('active_start') >= start && el.get('active_stop') <= end)) {
+                        start = el.get('active_start');
+                        end = el.get('active_stop');
+                        prevMover = el;
+                    } else {
+                        swal({
+                            title: 'Movers cannot be reconciled!',
+                            text: 'The mover: ' + el.get('name') + ' does not fall in the runtime of the previous mover: ' +
+                                prevMover.get('name') + '. You will need to either turn off this mover or extrapolate.',
+                            type: 'warning',
+                            showCancelButton: true,
+                            confirmButtonText: 'Select Option',
+                            cancelButtonText: 'Cancel'
+                        }).then(_.bind(function(option){
+                            if (option) {
+                                swal({
+                                    title: 'Choose correction option',
+                                    text: 'Select whether to turn off or extrapolate mover: ' + el.get('name') + '.',
+                                    type: 'warning',
+                                    showCancelButton: true,
+                                    confirmButtonText: 'Turn Off',
+                                    cancelButtonText: 'Extrapolate'
+                                }).then(_.bind(function(turn_off){
+                                    if (turn_off) {
+                                        el.set('on', false);
+                                    } else {
+                                        el.set('extrapolate', true);
+                                    }
+                                }, this));
+                            }
+                        }, this));
+                    }
+                }
+            }, this));
+
+            return {start: start, end: end};
         },
 
         getTimeInvalidModels: function() {
