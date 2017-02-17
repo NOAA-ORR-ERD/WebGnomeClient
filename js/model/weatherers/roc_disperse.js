9f0d16f100024cfac4af52ed350b3a8378c24bf2
Nathan Wilkins <nathan.wilkins@noaa.gov>
2017-02-17 14:35:56 -0800

    corrected attributes for disperse model

--- js/model/weatherers/roc_disperse.js
+++ js/model/weatherers/roc_disperse.js
@@ -7,7 +7,7 @@ define([
     var ROCDisperseModel = ROCWeatherer.extend({
         defaults: function(){
             return {
-                obj_type: 'gnome.weatherers.ROC.Disperse',
+                obj_type: 'gnome.weatherers.roc.Disperse',
                 name: 'ROC Disperse',
                 timeseries: this.calculateOperatingPeriods(),
                 transit: 10,
@@ -16,8 +16,8 @@ define([
                 platform_type: '',
                 pass_type: 'bidirectional',
                 dor: 20,
-                dosage: 'auto',
-                dosage_val: 5,
+                dosage_type: 'auto',
+                dosage: 0,
                 dispersant: '1',
                 dispersant_ef: 0,
                 loading_type: 'simultaneous',
