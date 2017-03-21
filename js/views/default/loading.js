bb0ed0c63d0bfbd4c577f0d5bd3fced4360664cc
Nathan Wilkins <nathan.wilkins@noaa.gov>
2017-03-21 11:07:34 -0700

    added 'processing model' to loading view when actually running a model

--- js/views/default/loading.js
+++ js/views/default/loading.js
@@ -6,13 +6,20 @@ define([
     'use strict';
     var loadingView = Backbone.View.extend({
         className: 'app-state loading',
-        initialize: function(){
+        initialize: function(options){
             this.render();
         },
 
         render: function(){
             if($('.app-state.loading').length === 0 && $('.loading-modal').length === 0){
-                this.$el.append('<div class="spinner"></div>Loading');
+                var text = '';
+                if(window.location.href.match(/trajectory|response|fate/)){
+                    text = 'Processing Model';
+                    this.$el.addClass('processing');
+                } else {
+                    text = 'Loading';
+                }
+                this.$el.append('<div class="spinner"></div>' + text);
                 $('body').append(this.$el);
             }
         }
