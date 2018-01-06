1e368a179b2de2273414f12f2de5fa39baa2e3f4
James L. Makela <james.makela@noaa.gov>
2018-01-06 12:59:38 -0800

    Issue #913: Prompt for name when saving model on server

---
+++ js/views/model/persist_model_modal.js
@@ -0,0 +1,30 @@
+define([
+    'jquery',
+    'underscore',
+    'views/modal/form',
+    'text!templates/form/persist_model.html'
+], function($, _,
+		    FormModal, PersistModelTemplate) {
+    var persistModelModalForm = FormModal.extend({
+        title: 'Save the Model on the Server',
+        className: 'modal form-modal persist-model-form',
+
+        initialize: function(options, modelFileName) {
+            this.modelFileName = modelFileName;
+            FormModal.prototype.initialize.call(this, options);
+        },
+
+        render: function(){
+            var persistModelTemplate = _.template(PersistModelTemplate);
+            this.body = _.template(persistModelTemplate({'model_name': this.modelFileName}));
+            FormModal.prototype.render.call(this);
+        },
+
+        save: function(){
+            this.modelFileName = this.$('#name')[0].value;
+            FormModal.prototype.save.call(this);
+        },
+    });
+
+    return persistModelModalForm;
+});
