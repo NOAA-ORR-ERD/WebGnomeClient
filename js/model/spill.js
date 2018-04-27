53e1dd84a4630b2dd34f609b7b0d5d45eac7b285
jay.hennen <jay.hennen@noaa.gov>
2018-04-27 11:48:58 -0700

    zero-mass particles get correct alpha of 0

--- js/model/spill.js
+++ js/model/spill.js
@@ -405,7 +405,7 @@ define([
             */
             var colormap = this.get('_appearance').get('colormap');
             var genColorwithAlpha = function(colorStr, alpha) {
-                return alpha ?
+                return _.isUndefined(alpha) ?
                     Cesium.Color.fromCssColorString(colorStr).withAlpha(alpha) :
                     Cesium.Color.fromCssColorString(colorStr);
             };
