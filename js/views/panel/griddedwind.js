aaa92234d886dfe17483575e201dfe138f1ecc58
JamesMakela <james.makela@gmail.com>
2018-06-07 22:08:30 -0700

    Created a gridded wind edit form that can toggle the data extrapolation.

--- js/views/panel/griddedwind.js
+++ js/views/panel/griddedwind.js
@@ -4,12 +4,14 @@ define([
     'backbone',
     'sweetalert',
     'ol',
-    'views/default/map',
-    'views/panel/base',
-    'views/form/griddedwind',
     'text!templates/panel/griddedwind.html',
-    'views/modal/form'
-], function($, _, Backbone, swal, ol, OlMapView, BasePanel, GriddedWindForm, GriddedWindPanelTemplate, FormModal){
+    'views/default/map',
+    'views/form/griddedwind_edit',
+    'views/panel/base',
+    'views/form/griddedwind'
+], function($, _, Backbone, swal, ol,
+            GriddedWindPanelTemplate,
+            OlMapView, GridedWindEditForm, BasePanel, GriddedWindForm) {
     var griddedWindPanel = BasePanel.extend({
         className: 'col-md-3 griddedwind object panel-view',
 
@@ -17,58 +19,74 @@ define([
             'gnome.movers.py_wind_movers.PyWindMover'
         ],
 
-        initialize: function(options){
+        initialize: function(options) {
             BasePanel.prototype.initialize.call(this, options);
-            this.listenTo(webgnome.model, 'change:duration chage:start_time', this.rerender);
-            this.listenTo(webgnome.model.get('movers'), 'add change remove', this.rerender);
+
+            this.listenTo(webgnome.model, 'change:duration chage:start_time',
+                          this.rerender);
+            this.listenTo(webgnome.model.get('movers'), 'add change remove',
+                          this.rerender);
         },
 
-        new: function(){
+        new: function() {
             var form = new GriddedWindForm();
             form.on('hidden', form.close);
-            form.on('save', _.bind(function(mover){
+
+            form.on('save', _.bind(function(mover) {
                 webgnome.model.get('movers').add(mover);
+
                 if (mover.attributes.obj_type === 'gnome.movers.py_wind_movers.PyWindMover') {
                     webgnome.model.get('environment').add(mover.get('wind'));
                 }
+
                 webgnome.model.save(null, {validate: false});
             }, this));
+
             form.render();
         },
 
-        edit: function(e){
+        edit: function(e) {
             e.stopPropagation();
             var id = this.getID(e);
 
             var griddedwind = webgnome.model.get('movers').get(id);
-            var griddedwindView = new FormModal({title: 'Edit Wind', model: griddedwind});
-            griddedwindView.on('save', function(){
+            var title = 'Gridded Wind: ' + griddedwind.get('name');
+
+            var griddedwindView = new GridedWindEditForm({title: title,
+                                                          model: griddedwind});
+
+            griddedwindView.on('save', function() {
                 griddedwindView.on('hidden', griddedwindView.close);
             });
+
             griddedwindView.on('wizardclose', griddedwindView.close);
+
             griddedwindView.render();
         },
 
-        render: function(){
-            var griddedwind = webgnome.model.get('movers').filter(function(mover){
-                return [
-                    'gnome.movers.py_wind_movers.PyWindMover'
-                ].indexOf(mover.get('obj_type')) !== -1;
+        render: function() {
+            var griddedwind = webgnome.model.get('movers').filter(function(mover) {
+                return (['gnome.movers.py_wind_movers.PyWindMover']
+                        .indexOf(mover.get('obj_type')) !== -1);
             });
+
             var compiled = _.template(GriddedWindPanelTemplate, {
                 griddedwind: griddedwind
             });
+
             this.$el.html(compiled);
 
             if (griddedwind.length > 0) {
                 this.$('.griddedwind .panel').addClass('complete');
                 this.$el.removeClass('col-md-3').addClass('col-md-6');
                 this.$('.panel-body').show();
-            } else {
+            }
+            else {
                 this.$el.removeClass('col-md-6').addClass('col-md-3');
                 this.$('.panel-body').hide();
             }
-            if(griddedwind.length > 0){
+
+            if (griddedwind.length > 0) {
                 this.$('.panel-body').show();
                 this.current_layers = new ol.Collection([
                     new ol.layer.Tile({
@@ -78,21 +96,27 @@ define([
                             })
                     })
                 ]);
-
-
-            } else {
+            }
+            else {
                 this.current_extents = [];
                 this.$('.panel-body').hide();
             }
+
             BasePanel.prototype.render.call(this);
         },
 
-        addWindToPanel: function(geojson){
-            if(geojson){
+        addWindToPanel: function(geojson) {
+            if (geojson) {
                 var gridSource = new ol.source.Vector({
-                    features: (new ol.format.GeoJSON()).readFeatures(geojson, {featureProjection: 'EPSG:3857'}),
+                    features: (new ol.format.GeoJSON()
+                               .readFeatures(geojson,
+                                             {featureProjection: 'EPSG:3857'})),
                 });
-                var extentSum = gridSource.getExtent().reduce(function(prev, cur){ return prev + cur;});
+
+                var extentSum = gridSource.getExtent()
+                                .reduce(function(prev, cur) {
+                                    return prev + cur;
+                                });
 
                 var gridLayer = new ol.layer.Image({
                     name: 'modelwind',
@@ -107,17 +131,19 @@ define([
                     })
                 });
 
-                if(!_.contains(this.current_extents, extentSum)){
+                if (!_.contains(this.current_extents, extentSum)) {
                     this.current_layers.push(gridLayer);
                     this.current_extents.push(extentSum);
                 }
             }
         },
 
-        delete: function(e){
+        delete: function(e) {
             e.stopPropagation();
+
             var id = this.getID(e);
             var spill = webgnome.model.get('movers').get(id);
+
             swal({
                 title: 'Delete "' + spill.get('name') + '"',
                 text: 'Are you sure you want to delete this wind?',
@@ -125,8 +151,8 @@ define([
                 confirmButtonText: 'Delete',
                 confirmButtonColor: '#d9534f',
                 showCancelButton: true
-            }).then(_.bind(function(isConfirmed){
-                if(isConfirmed){
+            }).then(_.bind(function(isConfirmed) {
+                if (isConfirmed) {
                     webgnome.model.get('movers').remove(id);
                     webgnome.model.save(null, {
                         validate: false
@@ -135,13 +161,14 @@ define([
             }, this));
         },
 
-        close: function(){
-            if(this.currentMap){
+        close: function() {
+            if (this.currentMap) {
                 this.currentMap.close();
             }
+
             BasePanel.prototype.close.call(this);
         }
-
     });
+
     return griddedWindPanel;
 });
