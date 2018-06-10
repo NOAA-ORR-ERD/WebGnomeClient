bc16315116dc8a31fb1d7fa94ab8ee58c3582944
JamesMakela <james.makela@gmail.com>
2018-06-10 15:19:31 -0700

    Modified the gridded current edit form so it  can toggle data extrapolation.

--- js/views/form/mover/grid.js
+++ js/views/form/mover/grid.js
@@ -6,18 +6,32 @@ define([
     'views/form/mover/base',
     'text!templates/form/mover/edit.html',
     'views/modal/form'
-], function($, _, Backbone, module, BaseMoverForm, FormTemplate, FormModal){
+], function($, _, Backbone, module, BaseMoverForm, FormTemplate, FormModal) {
     'use strict';
     var modelForm = BaseMoverForm.extend({
-        render: function(options){
+
+        events: function() {
+            return _.defaults({
+                'click div :checkbox': 'setExtrapolation',
+            }, BaseMoverForm.prototype.events);
+        },
+
+        render: function(options) {
             this.body = _.template(FormTemplate, {
                 name: this.model.get('name'),
                 active: this.model.get('on'),
-                scale_value: this.model.get('scale_value')
+                scale_value: this.model.get('scale_value'),
+                extrapolation_is_allowed: this.model.get('current').get('extrapolation_is_allowed')
             });
+
             FormModal.prototype.render.call(this, options);
-        }
+        },
+
+        setExtrapolation: function(e) {
+            var selected = $(e.target).is(':checked');
+            this.model.get('current').set('extrapolation_is_allowed', selected);
+        },
     });
-    
+
     return modelForm;
 });
