8938e929d08142b3ff3f4d92f866809607697db1
jay.hennen <jay.hennen@noaa.gov>
2018-09-14 13:21:01 -0700

    removed old toggle weatherers

--- js/model/gnome.js
+++ js/model/gnome.js
@@ -221,7 +221,6 @@ define([
 
         spillsChange: function(child){
             this.childChange('spills', child);
-            this.toggleWeatherers(child);
             if(this.get('uncertain')) {
                 this.set('uncertain', false);
                 this.save(null, {
@@ -340,27 +339,8 @@ define([
             }
         },
 
-        weatherersChange: function(child){
-
-        },
-
         outputtersChange: function(child){
             this.childChange('outputters', child);
-        },
-
-        configureWeatherers: function() {
-
-        },
-
-        toggleWeatherers: function(spillChild) {
-            var hasSubstance = this.configureWeatherers();
-            var weatherers = this.get('weatherers');
-
-            for (var i = 0; i < weatherers.models.length; i++) {
-                if (weatherers.at(i).get('on') !== hasSubstance) {
-                    weatherers.at(i).set('on', hasSubstance).save();
-                }
-            }
         },
 
         validateSpills: function() {
