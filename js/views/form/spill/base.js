271f04094112b01d0ae2310f53cc4a4c45da0f32
jay.hennen <jay.hennen@noaa.gov>
2019-04-03 14:18:26 -0700

    substance should now take on location file wizard

--- js/views/form/spill/base.js
+++ js/views/form/spill/base.js
@@ -440,7 +440,7 @@ define([
                 }
                 this.oilLibraryView = new OilLibraryView({}, subs);
                 this.oilLibraryView.render();
-                this.oilLibraryView.on('hidden', _.bind(function(){webgnome.model.setGlobalSubstance(subs);}, this));
+                this.oilLibraryView.on('hidden', _.bind(function(){this.model.set('substance', subs); webgnome.model.setGlobalSubstance(subs);}, this));
                 this.oilLibraryView.on('hidden', _.bind(this.show , this));
                 this.oilLibraryView.on('hidden', this.reloadOil, this);
                 this.oilLibraryView.on('hidden', this.tabStatusSetter, this);
