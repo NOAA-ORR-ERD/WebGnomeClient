5c2389cd49bb5bede7f818ab136adfe34b955bf6
jay.hennen <jay.hennen@noaa.gov>
2019-08-16 16:09:15 -0700

    various fixes to menus and file loading

--- js/model/visualization/mover_appearance.js
+++ js/model/visualization/mover_appearance.js
@@ -7,19 +7,16 @@ define([
     var moverAppearanceModel = BaseAppearance.extend({
         defaults: {
             vec_on: false,
-            grid_on: false,
             vec_color: '#9370DB', //MEDIUMPURPLE,
             vec_alpha: 0.7,
+            grid_on: false,
             grid_color: '#585858', //dark gray
             grid_alpha: 0.3,
-            alpha: 0.7,
-            scale: 1,
             ctrl_names: {
                 title: 'Mover Appearance',
                 vec_on: 'Show Arrows',
                 vec_color: 'Arrow Color',
                 vec_alpha: 'Arrow Alpha',
-                vec_scale: 'Arrow Scale',
                 grid_on: 'Show Grid',
                 grid_color: 'Grid Color',
                 grid_alpha: 'Grid Alpha'
