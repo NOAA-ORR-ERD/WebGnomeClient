92679a95db2acc163b40a749ba6026ddf6b69c8e
amy.macfadyen <amy.macfadyen@noaa.gov>
2021-09-10 12:51:04 -0700

    Added a check when adding a spill (using the map) for a 0->360 type map. In this case, the spill will use longitude in 0-->360 rather than the default -180->180

--- js/views/form/spill/map.js
+++ js/views/form/spill/map.js
@@ -111,8 +111,13 @@ define([
         droppedPinHandler: function(ent, coords) {
             //this context should always be the Form object
             var prev = this.model.get(ent.model_attr);
-            var SATest = this.model.testVsSpillableArea(coords, webgnome.model.get('map'));
-            var MBTest = this.model.testVsMapBounds(coords, webgnome.model.get('map'));
+            var map = webgnome.model.get('map')
+            var nswe = map.getBoundingRectangle_nswe();            
+            if (nswe[3] > 180 && coords[0]<0) {
+                coords[0] = coords[0] + 360;
+            }            
+            var SATest = this.model.testVsSpillableArea(coords, map);
+            var MBTest = this.model.testVsMapBounds(coords, map);
             if (!MBTest) {
                 //spill outside map bounds
                 this.error('Start or end position outside map bounds. Some or all particles may disapear immediately on release');
