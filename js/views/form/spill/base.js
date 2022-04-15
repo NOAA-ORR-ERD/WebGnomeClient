f208b03277433dd58b5c67cbcfb587d7a40718a9
amy.macfadyen <amy.macfadyen@noaa.gov>
2022-04-15 13:05:03 -0700

    Lock controls when processing oil json file

--- js/views/form/spill/base.js
+++ js/views/form/spill/base.js
@@ -535,7 +535,8 @@ define([
                     autoProcessQueue:true,
                     dictDefaultMessage: 'Drop file here to load an oil (or click to navigate). <br>Click the help icon for details on supported file formats.',
                 });
-                this.$('.substance-upload').append(this.dzone.$el);
+                this.$('.substance-upload').append(this.dzone.$el);               
+                this.dzone.dropzone.on("addedfile", _.bind(this.lockControls,this))
                 this.listenTo(this.dzone, 'upload_complete', _.bind(this.newloaded, this));
             }
         },
