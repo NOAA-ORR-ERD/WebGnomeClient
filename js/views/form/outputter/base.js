7c829aa9509b162922a0000726bdd24704011d64
Caitlin O'Connor <caitlin.oconnor@noaa.gov>
2023-04-28 10:53:05 -0700

    added checkbox for outputting single timestep

--- js/views/form/outputter/base.js
+++ js/views/form/outputter/base.js
@@ -18,7 +18,7 @@ define([
             CacheModel, KMZModel, NetCDFModel, ShapeModel, BinaryModel) {
     'use strict';
     var outputForm = BaseForm.extend({
-        
+
 
         initialize: function(options, model) {
             BaseForm.prototype.initialize.call(this, options);
@@ -30,50 +30,52 @@ define([
             var output_timestep_hrs = this.model.get('output_timestep')/3600;
             var zeroStep = this.model.get('output_zero_step');
             var lastStep = this.model.get('output_last_step');
+            var singleStep = this.model.get('output_single_step');
 
             var html = _.template(OutputTemplate)({
                 start_time: start_time,
                 output_timestep: output_timestep_hrs,
                 output_zero_step: zeroStep,
-                output_last_step: lastStep
+                output_last_step: lastStep,
+                output_single_step: singleStep
             });
 
             this.$el.append(html);
 
             this.contextualizeTime();
-           
+
             BaseForm.prototype.render.call(this);
-            
+
             this.$('#start_time').datetimepicker({
                 format: webgnome.config.date_format.datetimepicker,
                 allowTimes: webgnome.config.date_format.half_hour_times,
                 step: webgnome.config.date_format.time_step
             });
-            
+
             //this.$('.attributes').hide();
         },
 
         update: function(e) {
-            
+
             var name = this.$(e.currentTarget).attr('name');
             var value = this.$(e.currentTarget).val();
-            
+
             if (name === 'start_time') {
                 this.model.set('output_start_time', moment(value, webgnome.config.date_format.moment).format('YYYY-MM-DDTHH:mm:ss'));
             }
-            
+
             if (name === 'output_timestep_hrs') {
                 this.model.set('output_timestep', value * 3600);
             }
-            
+
             if ($(e.target).attr('type') === 'checkbox') {
                 value = e.target.checked;
                 this.model.set(name, value);
             }
-            
-                                
+
+
         },
-        
+
         contextualizeTime: function() {
             var timeInfo = this.model.timeConversion();
 
